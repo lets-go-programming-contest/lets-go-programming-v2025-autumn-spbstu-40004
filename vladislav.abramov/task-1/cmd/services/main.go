62f62c4e3bc1cce4c1f335b948f653e9ef19e70b
15446-rus75 <vladabramov06@yandex.ru>
2025-09-18 23:25:33 +0300

    [TASK-1] fix format

--- vladislav.abramov/task-1/cmd/services/main.go
+++ vladislav.abramov/task-1/cmd/services/main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
-  var a int
-  var b int
-  var op string
+	var a int
+	var b int
+	var op string
 
-  _, error1 := fmt.Scanln(&a)
-  if error1 != nil {
-    fmt.Print("Invalid first operand\n")
-    return
-  }
-  _, error2 := fmt.Scanln(&b)
-  if error2 != nil {
-    fmt.Print("Invalid second operand\n")
-    return
-  }
-  _, error3 := fmt.Scanln(&op)
-  if error3 != nil {
-    fmt.Print("Invalid operation\n")
-    return
-  }
-  if b == 0 && op == "/" {
-    fmt.Print("Division by zero\n")
-    return
-  }
+	_, error1 := fmt.Scanln(&a)
+	if error1 != nil {
+		fmt.Print("Invalid first operand\n")
+		return
+	}
+	_, error2 := fmt.Scanln(&b)
+	if error2 != nil {
+		fmt.Print("Invalid second operand\n")
+		return
+	}
+	_, error3 := fmt.Scanln(&op)
+	if error3 != nil {
+		fmt.Print("Invalid operation\n")
+		return
+	}
+	if b == 0 && op == "/" {
+		fmt.Print("Division by zero\n")
+		return
+	}
 
-  if op == "+" {
-    fmt.Println(a + b)
-  } else if op == "-" {
-    fmt.Println(a - b)
-  } else if op == "*" {
-    fmt.Println(a * b)
-  } else if op == "/" {
-    fmt.Println(a / b)
-  } else {
-    fmt.Print("Invalid operation\n")
-  }
+	if op == "+" {
+		fmt.Println(a + b)
+	} else if op == "-" {
+		fmt.Println(a - b)
+	} else if op == "*" {
+		fmt.Println(a * b)
+	} else if op == "/" {
+		fmt.Println(a / b)
+	} else {
+		fmt.Print("Invalid operation\n")
+	}
 }
