4d958621848f50341338efa28d8b9d427e200ab1
jambii1 <s.maslevtsov@gmail.com>
2025-09-18 19:13:04 +0300

    [TASK-1] Add operands input

--- stanislav.maslevtsov/task-1/cmd/service/main.go
+++ stanislav.maslevtsov/task-1/cmd/service/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	var operand1 int
+	_, scanErr := fmt.Scanln(&operand1)
+	if scanErr != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	var operand2 int
+	_, scanErr = fmt.Scanln(&operand2)
+	if scanErr != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+}
