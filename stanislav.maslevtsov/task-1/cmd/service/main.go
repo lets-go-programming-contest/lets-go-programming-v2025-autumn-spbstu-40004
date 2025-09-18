bfeb22d95919ca0b54eb9e20ccd66dc6c01e3bf0
jambii1 <s.maslevtsov@gmail.com>
2025-09-18 19:20:22 +0300

    [TASK-1] Add operation input, executing and result output

--- stanislav.maslevtsov/task-1/cmd/service/main.go
+++ stanislav.maslevtsov/task-1/cmd/service/main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+func safeDivision(operand1 int, operand2 int) (int, error) {
+	if operand2 == 0 {
+		return 0, errors.New("division by zero")
+	}
+	return operand1 / operand2, nil
+}
 
 func main() {
 	var operand1 int
@@ -15,5 +25,26 @@ func main() {
 	if scanErr != nil {
 		fmt.Println("Invalid second operand")
 		return
+	}
+
+	var operation string
+	fmt.Scanln(&operation)
+
+	switch operation {
+	case "+":
+		fmt.Println(operand1 + operand2)
+	case "-":
+		fmt.Println(operand1 - operand2)
+	case "*":
+		fmt.Println(operand1 * operand2)
+	case "/":
+		divisionResult, divisionErr := safeDivision(operand1, operand2)
+		if divisionErr != nil {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(divisionResult)
+	default:
+		fmt.Println("Invalid operation")
 	}
 }
