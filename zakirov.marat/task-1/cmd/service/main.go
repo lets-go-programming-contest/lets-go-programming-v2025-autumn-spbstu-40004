a98d1f012f2618cddb91393b526636e73379de32
ZakirovMS <mz.08.08.06@gmail.com>
2025-09-19 12:37:28 +0300

    [TASK-1] Implementation of calculating function

--- zakirov.marat/task-1/cmd/service/main.go
+++ zakirov.marat/task-1/cmd/service/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("task-1")
+}
+
+func calculate(num_1 int, num_2 int, operator byte) (int, error) {
+	switch operator {
+	case '+':
+		return num_1 + num_2, nil
+	case '-':
+		return num_1 - num_2, nil
+	case '*':
+		return num_1 * num_2, nil
+	case '/':
+		if num_2 == 0 {
+			return 0, errors.New("division by zero")
+		}
+
+		return num_1 / num_2, nil
+	default:
+		return 0, errors.New("invalid operator")
+	}
+
 }
