95f62623fb27db30befe534e7b53cd3d7804694e
ZakirovMS <mz.08.08.06@gmail.com>
2025-09-19 13:25:09 +0300

    [TASK-1] Fix of errors handling in main, replacing an operand from byte to string

--- zakirov.marat/task-1/cmd/service/main.go
+++ zakirov.marat/task-1/cmd/service/main.go
@@ -10,38 +10,46 @@ func main() {
 	var (
 		num_1    int
 		num_2    int
-		operator byte
+		operator string
 		result   int
 	)
 
-	n, err := fmt.Fscan(os.Stdin, &num_1, &num_2, &operator)
+	_, err := fmt.Fscanln(os.Stdin, &num_1)
 	if err != nil {
-		switch n {
-		case 0:
-			fmt.Println("Invalid first operand")
-		case 1:
-			fmt.Println("Invalid second operand")
-		}
-
+		fmt.Println("Invalid first operand")
 		return
 	}
+
+	_, err = fmt.Fscanln(os.Stdin, &num_2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Fscanln(os.Stdin, &operator)
+	if err != nil {
+		fmt.Println("Invalid third operand")
+		return
+	}
+
 	result, err = calculate(num_1, num_2, operator)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
 	fmt.Println(result)
 }
 
-func calculate(operand_1 int, operand_2 int, operator byte) (int, error) {
+func calculate(operand_1 int, operand_2 int, operator string) (int, error) {
 	switch operator {
-	case '+':
+	case "+":
 		return operand_1 + operand_2, nil
-	case '-':
+	case "-":
 		return operand_1 - operand_2, nil
-	case '*':
+	case "*":
 		return operand_1 * operand_2, nil
-	case '/':
+	case "/":
 		if operand_2 == 0 {
 			return 0, errors.New("Division by zero")
 		}
@@ -50,5 +58,4 @@ func calculate(operand_1 int, operand_2 int, operator byte) (int, error) {
 	default:
 		return 0, errors.New("Invalid operation")
 	}
-
 }
