3273a097a6388a5ac205a7580996c5ba5e744d95
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2025-09-19 15:44:54 +0300

    [TASK-1] Added user interaction

--- veniamin.belyaev/task-1/cmd/service/main.go
+++ veniamin.belyaev/task-1/cmd/service/main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var firstNum, secondNum, result int
+	var operator string
+
+	fmt.Println("Type the first number: ")
+	_, errFirstNum := fmt.Scanln(&firstNum)
+
+	fmt.Println("Type the second number: ")
+	_, errSecondNum := fmt.Scanln(&secondNum)
+
+	fmt.Println("Type an operator (+, -, *, /): ")
+	_, errOperator := fmt.Scanln(&operator)
+
+	if errFirstNum != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	if errSecondNum != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	if errOperator != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch operator {
+	case "+":
+		result = firstNum + secondNum
+	case "-":
+		result = firstNum - secondNum
+	case "/":
+		if secondNum == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		result = firstNum / secondNum
+	case "*":
+		result = firstNum * secondNum
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	fmt.Println(result)
 }
