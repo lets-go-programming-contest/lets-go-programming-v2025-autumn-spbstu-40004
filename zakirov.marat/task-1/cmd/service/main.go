2bb43788fec58c41c8dbb556614d52d4ca659abc
ZakirovMS <mz.08.08.06@gmail.com>
2025-09-19 13:17:15 +0300

    [TASK-1] Main function implementation

--- zakirov.marat/task-1/cmd/service/main.go
+++ zakirov.marat/task-1/cmd/service/main.go
@@ -3,28 +3,52 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("task-1")
-}
+	var (
+		num_1    int
+		num_2    int
+		operator byte
+		result   int
+	)
 
-func calculate(num_1 int, num_2 int, operator byte) (int, error) {
-	switch operator {
-	case '+':
-		return num_1 + num_2, nil
-	case '-':
-		return num_1 - num_2, nil
-	case '*':
-		return num_1 * num_2, nil
-	case '/':
-		if num_2 == 0 {
-			return 0, errors.New("division by zero")
+	n, err := fmt.Fscan(os.Stdin, &num_1, &num_2, &operator)
+	if err != nil {
+		switch n {
+		case 0:
+			fmt.Println("Invalid first operand")
+		case 1:
+			fmt.Println("Invalid second operand")
 		}
 
-		return num_1 / num_2, nil
+		return
+	}
+	result, err = calculate(num_1, num_2, operator)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
+}
+
+func calculate(operand_1 int, operand_2 int, operator byte) (int, error) {
+	switch operator {
+	case '+':
+		return operand_1 + operand_2, nil
+	case '-':
+		return operand_1 - operand_2, nil
+	case '*':
+		return operand_1 * operand_2, nil
+	case '/':
+		if operand_2 == 0 {
+			return 0, errors.New("Division by zero")
+		}
+
+		return operand_1 / operand_2, nil
 	default:
-		return 0, errors.New("invalid operator")
+		return 0, errors.New("Invalid operation")
 	}
 
 }
