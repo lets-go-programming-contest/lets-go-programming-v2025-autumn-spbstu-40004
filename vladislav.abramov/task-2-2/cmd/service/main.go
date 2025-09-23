4ccfc132adf558c4d16e8bedbbdddc8ee115aaef
15446-rus75 <vladabramov06@yandex.ru>
2025-09-23 18:34:57 +0300

    [TASK-2-2] some lint fixes and interface logic fix

--- vladislav.abramov/task-2-2/cmd/service/main.go
+++ vladislav.abramov/task-2-2/cmd/service/main.go
@@ -7,20 +7,22 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
+func (h *IntHeap) Len() int {
+	return len(*h)
 }
 
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] > h[j]
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *IntHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
+	if num, good := x.(int); good {
+		*h = append(*h, num)
+	}
 }
 
 func (h *IntHeap) Pop() interface{} {
@@ -28,30 +30,31 @@ func (h *IntHeap) Pop() interface{} {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
 
 func main() {
 	var nCount, kCount int
 
-  _, err := fmt.Scan(&nCount)
-  if err != nil {
-    fmt.Print("Failed to read N\n")
-  }
+	_, err := fmt.Scan(&nCount)
+	if err != nil {
+		fmt.Print("Failed to read N\n")
+	}
 
 	arr := make([]int, nCount)
 
 	for count := range nCount {
 		_, err = fmt.Scan(&arr[count])
-    if err != nil {
-      fmt.Print("Failed to read data\n")
-    }
+		if err != nil {
+			fmt.Print("Failed to read data\n")
+		}
 	}
 
 	_, err = fmt.Scan(&kCount)
-  if err != nil {
-    fmt.Print("Failed to read K\n")
-  }
+	if err != nil {
+		fmt.Print("Failed to read K\n")
+	}
 
 	heapOfMeals := &IntHeap{}
 	heap.Init(heapOfMeals)
@@ -63,7 +66,9 @@ func main() {
 	var result int
 
 	for range kCount {
-		result = heap.Pop(heapOfMeals).(int)
+		if value, good := heap.Pop(heapOfMeals).(int); good {
+			result = value
+		}
 	}
 
 	fmt.Println(result)
