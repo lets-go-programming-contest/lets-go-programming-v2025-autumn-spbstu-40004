931c627284593a7d969161aa290d1a6f9dffb2f8
ZakirovMS <mz.08.08.06@gmail.com>
2025-09-23 12:14:43 +0300

    [TASK-1] Avoiding error handling, getting rid of the calculate function

--- zakirov.marat/task-1/cmd/service/main.go
+++ zakirov.marat/task-1/cmd/service/main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 )
 
 func main() {
 	var (
-		num_1    int
-		num_2    int
+		numSt    int
+		numNd    int
 		operator string
-		result   int
 	)
 
-	_, err := fmt.Fscanln(os.Stdin, &num_1)
+	_, err := fmt.Fscanln(os.Stdin, &numSt)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
 
-	_, err = fmt.Fscanln(os.Stdin, &num_2)
+	_, err = fmt.Fscanln(os.Stdin, &numNd)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
@@ -32,30 +30,20 @@ func main() {
 		return
 	}
 
-	result, err = calculate(num_1, num_2, operator)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(result)
-}
-
-func calculate(operand_1 int, operand_2 int, operator string) (int, error) {
 	switch operator {
 	case "+":
-		return operand_1 + operand_2, nil
+		fmt.Println(numSt + numNd)
 	case "-":
-		return operand_1 - operand_2, nil
+		fmt.Println(numSt - numNd)
 	case "*":
-		return operand_1 * operand_2, nil
+		fmt.Println(numSt * numNd)
 	case "/":
-		if operand_2 == 0 {
-			return 0, errors.New("Division by zero")
+		if numSt == 0 {
+			fmt.Println("Division by zero")
 		}
 
-		return operand_1 / operand_2, nil
+		fmt.Println(numSt / numNd)
 	default:
-		return 0, errors.New("Invalid operation")
+		fmt.Println("Invalid operation")
 	}
 }
