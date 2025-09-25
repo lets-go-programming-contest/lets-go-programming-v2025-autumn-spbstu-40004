c78c1864f874d20c8b54aa312511f0bd34d88ca7
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2025-09-26 01:06:13 +0300

    [TASK-2-2] Task implementation

--- veniamin.belyaev/task-2-2/cmd/service/main.go
+++ veniamin.belyaev/task-2-2/cmd/service/main.go
@@ -1,4 +1,75 @@
 package main
 
+import (
+	"container/heap"
+	"fmt"
+)
+
+type MaxHeap []int
+
+func (h MaxHeap) Len() int {
+	return len(h)
+}
+
+func (h MaxHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
+
+func (h MaxHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *MaxHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func initHeap(array []int) *MaxHeap {
+	maxHeap := &MaxHeap{}
+	*maxHeap = array
+	heap.Init(maxHeap)
+	return maxHeap
+}
+
 func main() {
+	var amount, k, result int
+
+	_, err := fmt.Scan(&amount)
+	if err != nil {
+		fmt.Println("Invalid amount")
+
+		return
+	}
+
+	mealArray := make([]int, amount)
+	for index := range amount {
+		_, err = fmt.Scan(&mealArray[index])
+		if err != nil {
+			fmt.Println("Invalid data")
+
+			return
+		}
+	}
+
+	_, err = fmt.Scan(&k)
+	if err != nil {
+		fmt.Println("Invalid k")
+
+		return
+	}
+
+	mealHeap := initHeap(mealArray)
+
+	for range k {
+		result = heap.Pop(mealHeap).(int)
+	}
+
+	fmt.Println(result)
 }
