a8b786931cc8c3774c4b3a01608844db34fbc66d
jambii1 <s.maslevtsov@gmail.com>
2025-09-25 22:43:01 +0300

    [TASK-2-1] Slice instead of array

--- stanislav.maslevtsov/task-2-1/cmd/service/main.go
+++ stanislav.maslevtsov/task-2-1/cmd/service/main.go
@@ -18,7 +18,10 @@ func main() {
 	}
 
 	for range departmentNum {
-		temperatures := [2]uint{15, 30}
+		temperature_borders := make([]uint, 2)
+		temperature_borders[0] = 15
+		temperature_borders[1] = 30
+
 		_, err = fmt.Scan(&employeeNum)
 		if err != nil {
 			fmt.Println("Error: invalid employee number")
@@ -36,31 +39,31 @@ func main() {
 
 			switch cmpOperator {
 			case ">=":
-				if border > temperatures[1] {
+				if border > temperature_borders[1] {
 					fmt.Println(-1)
 
 					continue
 				}
-				if border > temperatures[0] {
-					temperatures[0] = border
+				if border > temperature_borders[0] {
+					temperature_borders[0] = border
 				}
 			case "<=":
-				if border < temperatures[0] {
+				if border < temperature_borders[0] {
 					fmt.Println(-1)
 
 					continue
 				}
-				if border < temperatures[1] {
-					temperatures[1] = border
+				if border < temperature_borders[1] {
+					temperature_borders[1] = border
 				}
-				temperatures[1] = border
+				temperature_borders[1] = border
 			default:
 				fmt.Println("Error: invalid compare operator")
 
 				return
 			}
 
-			fmt.Println(temperatures[0])
+			fmt.Println(temperature_borders[0])
 		}
 	}
 }
