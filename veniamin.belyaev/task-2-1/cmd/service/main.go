228b70cb3bb2093dfef278dc7bc65ebb7e2a2f50
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2025-09-26 16:34:43 +0300

    [TASK-2-1] Cyclop workaround v2

--- veniamin.belyaev/task-2-1/cmd/service/main.go
+++ veniamin.belyaev/task-2-1/cmd/service/main.go
@@ -7,11 +7,6 @@ import (
 
 var errOperation = errors.New("invalid operation")
 
-// cyclomatic complexity workaround
-func isValueInInterval(value int, intervalMin int, intervalMax int) bool {
-	return intervalMin <= value && value <= intervalMax
-}
-
 func adjustTemperature(lowTemp int, highTemp int, askingTemp int, operation string) (int, int, error) {
 	if lowTemp == -1 && highTemp == -1 {
 		return lowTemp, highTemp, nil
@@ -22,14 +17,14 @@ func adjustTemperature(lowTemp int, highTemp int, askingTemp int, operation stri
 		if askingTemp > highTemp {
 			lowTemp = -1
 			highTemp = -1
-		} else if isValueInInterval(askingTemp, lowTemp, highTemp) {
+		} else if lowTemp <= askingTemp && askingTemp <= highTemp {
 			lowTemp = askingTemp
 		}
 	case "<=":
 		if askingTemp < lowTemp {
 			lowTemp = -1
 			highTemp = -1
-		} else if isValueInInterval(askingTemp, lowTemp, highTemp) {
+		} else if lowTemp <= askingTemp && askingTemp <= highTemp {
 			highTemp = askingTemp
 		}
 	default:
@@ -39,19 +34,44 @@ func adjustTemperature(lowTemp int, highTemp int, askingTemp int, operation stri
 	return lowTemp, highTemp, nil
 }
 
+func processDepartment(employeeAmount int, minTemp int, maxTemp int) {
+	var (
+		operation  string
+		askingTemp int
+	)
+
+	lowTemp := minTemp
+	highTemp := maxTemp
+
+	for range employeeAmount {
+		_, err := fmt.Scanln(&operation, &askingTemp)
+		if err != nil || askingTemp < 15 || askingTemp > 30 {
+			fmt.Println("Invalid employee input")
+
+			return
+		}
+
+		lowTemp, highTemp, err = adjustTemperature(lowTemp, highTemp, askingTemp, operation)
+		if err != nil {
+			fmt.Println(err)
+
+			return
+		}
+
+		fmt.Println(lowTemp)
+	}
+}
+
 func main() {
 	const (
 		minTemp = 15
 		maxTemp = 30
 	)
 
-	var (
-		departmentAmount, employeeAmount, askingTemp int
-		operation                                    string
-	)
+	var departmentAmount, employeeAmount int
 
 	_, err := fmt.Scanln(&departmentAmount)
-	if err != nil || !isValueInInterval(departmentAmount, 1, 1000) {
+	if err != nil || departmentAmount < 1 || departmentAmount > 1000 {
 		fmt.Println("Invalid department amount")
 
 		return
@@ -59,31 +79,12 @@ func main() {
 
 	for range departmentAmount {
 		_, err = fmt.Scanln(&employeeAmount)
-		if err != nil || !isValueInInterval(employeeAmount, 1, 1000) {
+		if err != nil || employeeAmount < 1 || employeeAmount > 1000 {
 			fmt.Println("Invalid employee amount")
 
 			return
 		}
 
-		lowTemp := minTemp
-		highTemp := maxTemp
-
-		for range employeeAmount {
-			_, err = fmt.Scanln(&operation, &askingTemp)
-			if err != nil || !isValueInInterval(askingTemp, 15, 30) {
-				fmt.Println("Invalid employee input")
-
-				return
-			}
-
-			lowTemp, highTemp, err = adjustTemperature(lowTemp, highTemp, askingTemp, operation)
-			if err != nil {
-				fmt.Println(err)
-
-				return
-			}
-
-			fmt.Println(lowTemp)
-		}
+		processDepartment(employeeAmount, minTemp, maxTemp)
 	}
 }
