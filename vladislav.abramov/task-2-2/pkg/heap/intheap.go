e26e691d5531cc4bcd43f3883d0071b326f26565
15446-rus75 <vladabramov06@yandex.ru>
2025-09-26 15:11:05 +0300

    [TASK-2-2] some format fixes

--- vladislav.abramov/task-2-2/pkg/heap/intheap.go
+++ vladislav.abramov/task-2-2/pkg/heap/intheap.go
@@ -5,35 +5,35 @@ import "container/heap"
 type IntHeap []int
 
 func (h *IntHeap) Len() int {
-  return len(*h)
+	return len(*h)
 }
 
 func (h *IntHeap) Less(i, j int) bool {
-  return (*h)[i] > (*h)[j]
+	return (*h)[i] > (*h)[j]
 }
 
 func (h *IntHeap) Swap(i, j int) {
-  (*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *IntHeap) Push(x interface{}) {
-  if num, good := x.(int); good {
-    *h = append(*h, num)
-  }
+	if num, good := x.(int); good {
+		*h = append(*h, num)
+	}
 }
 
 func (h *IntHeap) Pop() interface{} {
-  old := *h
-  n := len(old)
-  x := old[n-1]
-  *h = old[0 : n-1]
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
 
-  return x
+	return x
 }
 
 func NewIntHeap() *IntHeap {
-  h := &IntHeap{}
-  heap.Init(h)
+	h := &IntHeap{}
+	heap.Init(h)
 
-  return h
+	return h
 }
