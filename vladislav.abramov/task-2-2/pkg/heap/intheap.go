8354ecbaeb371c52b9ac9757f879905493a7fe38
15446-rus75 <vladabramov06@yandex.ru>
2025-09-26 15:09:42 +0300

    [TASK-2-2] add pkg

---
+++ vladislav.abramov/task-2-2/pkg/heap/intheap.go
@@ -0,0 +1,39 @@
+package heap
+
+import "container/heap"
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int {
+  return len(*h)
+}
+
+func (h *IntHeap) Less(i, j int) bool {
+  return (*h)[i] > (*h)[j]
+}
+
+func (h *IntHeap) Swap(i, j int) {
+  (*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+  if num, good := x.(int); good {
+    *h = append(*h, num)
+  }
+}
+
+func (h *IntHeap) Pop() interface{} {
+  old := *h
+  n := len(old)
+  x := old[n-1]
+  *h = old[0 : n-1]
+
+  return x
+}
+
+func NewIntHeap() *IntHeap {
+  h := &IntHeap{}
+  heap.Init(h)
+
+  return h
+}
