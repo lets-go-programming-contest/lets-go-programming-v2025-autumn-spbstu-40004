a72bf4ff36e519fc95707de586dedaba8dc3fb0d
Ksenia-rgb <ksenialymova@mail.ru>
2025-09-29 01:31:55 +0300

    [TASK-2-1] fix linter errors, move output after end of input

--- ksenia.alymova/task-2-1/cmd/service/main.go
+++ ksenia.alymova/task-2-1/cmd/service/main.go
@@ -10,6 +10,7 @@ func main() {
 		maxTemp = 30
 		minTemp = 15
 		cntUnit = 0
+		res     = make([]int, 0)
 	)
 
 	_, err := fmt.Scan(&cntUnit)
@@ -18,9 +19,11 @@ func main() {
 
 		return
 	}
+
 	for range cntUnit {
-		var cntWorker int = 0
-		_, err := fmt.Scan(&cntWorker)
+		cntWorker := 0
+		_, err = fmt.Scan(&cntWorker)
+
 		if err != nil || cntWorker < 1 || cntWorker > 1000 {
 			fmt.Println("Incorrect input")
 
@@ -39,7 +42,13 @@ func main() {
 				tempValue  int
 			)
 
-			fmt.Scan(&comparator)
+			_, err = fmt.Scan(&comparator)
+			if err != nil {
+				fmt.Println("Incorrect input")
+
+				return
+			}
+
 			_, err = fmt.Scan(&tempValue)
 			if err != nil {
 				fmt.Println("Incorrect input")
@@ -50,25 +59,30 @@ func main() {
 			switch {
 			case strings.Compare(comparator, ">=") == 0:
 				if tempValue > temperature[len(temperature)-1] {
-					fmt.Println("-1")
+					res = append(res, -1)
 				} else if tempValue > temperature[0] {
 					temperature = temperature[tempValue-temperature[0]:]
+					res = append(res, temperature[0])
+				} else {
+					res = append(res, temperature[0])
 				}
-
-				fmt.Println(temperature[0])
 			case strings.Compare(comparator, "<=") == 0:
 				if tempValue < temperature[0] {
-					fmt.Println("-1")
+					res = append(res, -1)
 				} else if tempValue < temperature[len(temperature)-1] {
 					temperature = temperature[:tempValue-temperature[0]+1]
+					res = append(res, temperature[0])
+				} else {
+					res = append(res, temperature[0])
 				}
-
-				fmt.Println(temperature[0])
 			default:
 				fmt.Println("Incorrect input")
 
 				return
 			}
 		}
+	}
+	for indexRes := range res {
+		fmt.Println(res[indexRes])
 	}
 }
