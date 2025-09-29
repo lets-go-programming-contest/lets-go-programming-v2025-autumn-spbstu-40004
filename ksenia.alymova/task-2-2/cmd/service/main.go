8617b199e6164466c8b44868c0bcda9ea71bcb86
Ksenia-rgb <ksenialymova@mail.ru>
2025-09-29 22:57:56 +0300

    [TASK-2-2] add first try of program, package for MaxHeap

--- ksenia.alymova/task-2-2/cmd/service/main.go
+++ ksenia.alymova/task-2-2/cmd/service/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"container/heap"
+	"errors"
+	"fmt"
+
+	"github.com/Ksenia-rgb/task-2-2/internal/maxheap"
+)
+
+var errInput = errors.New("incorrect input")
 
 func main() {
-	fmt.Println("Initial commit")
+	var cntDish int
+	heapDish := &maxheap.MaxHeap{}
+	heap.Init(heapDish)
+
+	_, err := fmt.Scanln(&cntDish)
+	if err != nil || cntDish < 1 || cntDish > 10000 {
+		fmt.Println(errInput)
+
+		return
+	}
+
+	for range cntDish {
+		var valueDish int
+
+		_, err = fmt.Scan(&valueDish)
+		if err != nil || valueDish < -10000 || valueDish > 10000 {
+			fmt.Println(errInput)
+
+			return
+		}
+
+		heap.Push(heapDish, valueDish)
+	}
+
+	var ratingDish int
+
+	_, err = fmt.Scan(&ratingDish)
+	if err != nil || ratingDish < 1 || ratingDish > heapDish.Len() {
+		fmt.Println(errInput)
+
+		return
+	}
+
+	for range ratingDish - 1 {
+		heap.Pop(heapDish)
+	}
+
+	fmt.Println(heap.Pop(heapDish))
 }
---
+++ ksenia.alymova/task-2-2/internal/maxheap/maxheap.go
@@ -0,0 +1,26 @@
+package maxheap
+
+type MaxHeap []int
+
+func (maxHeap MaxHeap) Len() int {
+	return len(maxHeap)
+}
+
+func (maxHeap MaxHeap) Less(indexLhs, indexRhs int) bool {
+	return maxHeap[indexLhs] >= maxHeap[indexRhs]
+}
+
+func (maxHeap MaxHeap) Swap(indexLhs, indexRhs int) {
+	maxHeap[indexLhs], maxHeap[indexRhs] = maxHeap[indexRhs], maxHeap[indexLhs]
+}
+
+func (maxHeap *MaxHeap) Push(value any) {
+	*maxHeap = append(*maxHeap, value.(int))
+}
+
+func (maxHeap *MaxHeap) Pop() any {
+	oldLen := len(*maxHeap)
+	returnValue := (*maxHeap)[oldLen-1]
+	*maxHeap = (*maxHeap)[:oldLen-1]
+	return returnValue
+}
