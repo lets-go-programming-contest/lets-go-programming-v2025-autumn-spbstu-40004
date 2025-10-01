4f53a1e2f18ce10c7421062a083578fe7d18bc82
Voronina <nadia.v.voronina@gmail.com>
2025-10-01 20:22:06 +0300

    [TASK-1] Better names and correct format

--- nadia.voronina/task-1/cmd/service/main.go
+++ nadia.voronina/task-1/cmd/service/main.go
@@ -1,51 +1,51 @@
 package main
 
 import (
-    "fmt"
-    "bufio"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-    var first_operand int
-    _, err_first := fmt.Scan(&first_operand)
-    if err_first != nil {
-        fmt.Println("Invalid first operand")
-        bufio.NewReader(os.Stdin).ReadString('\n')
-        return
-    }
-    
-    var second_operand int
-    _, err_second := fmt.Scan(&second_operand)
-    if err_second != nil {
-        fmt.Println("Invalid second operand")
-        bufio.NewReader(os.Stdin).ReadString('\n')
-        return
-    }
-    
-    var operator string
-    _, _ = fmt.Scan(&operator)
-    /*if err_op != nil {
-        fmt.Println("Invalid operator")
-    }*/
-    
-    switch operator {
-    
-    case "+":
-        fmt.Println(first_operand + second_operand)
-    case "-":
-        fmt.Println(first_operand - second_operand)
-    case "*":
-        fmt.Println(first_operand * second_operand)
-    case "/":
-        if second_operand == 0 {
-            fmt.Println("Division by zero")
-        } else {
-            fmt.Println(first_operand / second_operand)
-        }
-    default:
-        fmt.Println("Invalid operator")
-        bufio.NewReader(os.Stdin).ReadString('\n')
-        return
-    }
+	var firstOperand int
+	_, errFirst := fmt.Scan(&firstOperand)
+	if errFirst != nil {
+		fmt.Println("Invalid first operand")
+		_, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+		return
+	}
+
+	var secondOperand int
+	_, errSecond := fmt.Scan(&secondOperand)
+	if errSecond != nil {
+		fmt.Println("Invalid second operand")
+		_, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+		return
+	}
+
+	var operator string
+	_, _ = fmt.Scan(&operator)
+	/*if err_op != nil {
+	    fmt.Println("Invalid operator")
+	}*/
+
+	switch operator {
+
+	case "+":
+		fmt.Println(firstOperand + secondOperand)
+	case "-":
+		fmt.Println(firstOperand - secondOperand)
+	case "*":
+		fmt.Println(firstOperand * secondOperand)
+	case "/":
+		if secondOperand == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(firstOperand / secondOperand)
+		}
+	default:
+		fmt.Println("Invalid operator")
+		_, _ = bufio.NewReader(os.Stdin).ReadString('\n')
+		return
+	}
 }
