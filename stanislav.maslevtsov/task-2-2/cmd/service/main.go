112aa968ed0d96a33731adac5c3690837441242b
jambii1 <s.maslevtsov@gmail.com>
2025-10-02 18:28:02 +0300

    [TASK-2-2] Add dishes amount input

--- stanislav.maslevtsov/task-2-2/cmd/service/main.go
+++ stanislav.maslevtsov/task-2-2/cmd/service/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		dishesAmount int
+	)
+
+	_, err := fmt.Scan(&dishesAmount)
+	if err != nil {
+		fmt.Println("invalid number of dishes")
+
+		return
+	}
+}
