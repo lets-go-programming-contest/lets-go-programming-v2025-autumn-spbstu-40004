fd2783c97795d073f9307abdeb4b9673d11fb8bb
jambii1 <s.maslevtsov@gmail.com>
2025-10-02 18:40:43 +0300

    [TASK-2-2] Add MinHeap type and interface implementation

--- stanislav.maslevtsov/task-2-2/cmd/service/main.go
+++ stanislav.maslevtsov/task-2-2/cmd/service/main.go
@@ -4,6 +4,32 @@ import (
 	"fmt"
 )
 
+type MinHeap []uint
+
+func (heap MinHeap) Len() int {
+	return len(heap)
+}
+
+func (heap MinHeap) Less(left, right int) bool {
+	return heap[left] < heap[right]
+}
+
+func (heap MinHeap) Swap(left, right int) {
+	heap[left], heap[right] = heap[right], heap[left]
+}
+
+func (heap *MinHeap) Push(value any) {
+	*heap = append(*heap, value.(uint))
+}
+
+func (heap *MinHeap) Pop() any {
+	oldHeap := *heap
+	heapLen := len(oldHeap)
+	lastValue := oldHeap[heapLen-1]
+	*heap = oldHeap[0 : heapLen-1]
+	return lastValue
+}
+
 func main() {
 	var (
 		dishesAmount int
