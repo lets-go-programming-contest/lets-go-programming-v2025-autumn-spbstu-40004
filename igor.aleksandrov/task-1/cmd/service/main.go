21826e7ca4c1bc38cae64213df7dcf03fd641fbf
MrMels625 <MrMels625@gmail.com>
2025-10-03 20:25:10 +0300

    [TASK-1] scanning check added using only err return value

--- igor.aleksandrov/task-1/cmd/service/main.go
+++ igor.aleksandrov/task-1/cmd/service/main.go
@@ -4,10 +4,18 @@ import "fmt"
 
 func main() {
 	var lhs int
-	fmt.Scanln(&lhs)
+	_, err := fmt.Scanln(&lhs)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
 
 	var rhs int
-	fmt.Scanln(&rhs)
+	_, err = fmt.Scanln(&rhs)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
 
 	fmt.Println(lhs + rhs)
 }
