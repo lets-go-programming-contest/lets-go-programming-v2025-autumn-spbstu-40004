3a05d6b81724e845958a7b96c33299c4a932c684
ZakirovMS <mz.08.08.06@gmail.com>
2025-10-06 17:53:54 +0300

    [TASK-2-1] Getting numbers in a loop

--- marat.zakirov/task-2-1/cmd/service/main.go
+++ marat.zakirov/task-2-1/cmd/service/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		departNum int
+		employNum int
+		currTemp  int
+		newTemp   int
+		operator  string
+	)
+
+	fmt.Scan(&departNum)
+
+	for dep := 0; dep < departNum; dep++ {
+		fmt.Scan(&employNum)
+		currTemp = 0
+		for empl := 0; empl < employNum; empl++ {
+			fmt.Scan(&operator)
+			fmt.Scan(&newTemp)
+		}
+	}
 }
