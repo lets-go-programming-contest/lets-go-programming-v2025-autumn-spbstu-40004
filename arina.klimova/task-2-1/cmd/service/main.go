f363fc2985c24e8eaca3f7df02e5fee4ea30cdff
arinaklimova <arina.klim.06@mail.ru>
2025-10-06 13:28:18 +0300

    [TASK-2-1] personal temperature processing

--- arina.klimova/task-2-1/cmd/service/main.go
+++ arina.klimova/task-2-1/cmd/service/main.go
@@ -9,5 +9,27 @@ func main() {
 	fmt.Scanln(&N)
 	for i := 0; i < N; i++ {
 		fmt.Scanln(&K)
+		var maxTemp int = 30
+		var minTemp int = 15
+		for j := 0; j < K; j++ {
+			var op string
+			var personTemp int
+			fmt.Scan(&op, &personTemp)
+			if op == ">=" {
+				if personTemp >= minTemp && personTemp <= maxTemp {
+					minTemp = personTemp
+				}
+			} else if op == "<=" {
+				if personTemp <= maxTemp && personTemp >= minTemp {
+					maxTemp = personTemp
+				}
+			}
+			if maxTemp >= personTemp && minTemp <= personTemp {
+				fmt.Println(minTemp)
+			} else {
+				fmt.Println("-1")
+				return
+			}
+		}
 	}
 }
