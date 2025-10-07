135c07407e39a70430df17fb12763007cb4de713
ZakirovMS <mz.08.08.06@gmail.com>
2025-10-07 17:48:23 +0300

    [TASK-2-1] First full implementation of programm logic

--- marat.zakirov/task-2-1/cmd/service/main.go
+++ marat.zakirov/task-2-1/cmd/service/main.go
@@ -6,19 +6,37 @@ func main() {
 	var (
 		departNum int
 		employNum int
-		currTemp  int
 		newTemp   int
 		operator  string
 	)
 
-	fmt.Scan(&departNum)
+	leftBorder, rightBorder := 0, 30
 
+	fmt.Scan(&departNum)
 	for dep := 0; dep < departNum; dep++ {
 		fmt.Scan(&employNum)
-		currTemp = 0
 		for empl := 0; empl < employNum; empl++ {
 			fmt.Scan(&operator)
 			fmt.Scan(&newTemp)
+			if rightBorder == -1 {
+				continue
+			}
+
+			switch operator {
+			case "<=":
+				rightBorder = newTemp
+			case ">=":
+				leftBorder = newTemp
+			}
+
+			if rightBorder < leftBorder {
+				rightBorder = -1
+				leftBorder = -1
+				fmt.Println("-1")
+				continue
+			}
+
+			fmt.Println((rightBorder + leftBorder) / 2)
 		}
 	}
 }
