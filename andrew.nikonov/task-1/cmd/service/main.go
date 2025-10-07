a04f51118a072f2fdc126b2bccaa1b1d72e3374b
ysffmn <andrewnikonov387@gmail.com>
2025-10-07 22:25:04 +0300

    [TASK-1] basic logic implemented

--- andrew.nikonov/task-1/cmd/service/main.go
+++ andrew.nikonov/task-1/cmd/service/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var operand1, operand2 int
+	var operator string
+	cnt, err := fmt.Scan(&operand1, &operand2, &operator)
+	if err != nil {
+		switch cnt {
+		case 0:
+			fmt.Println("Invalid first operand")
+		case 1:
+			fmt.Println("Invalid second operand")
+		}
+	}
+	switch operator {
+	case "+":
+		//
+	case "-":
+		//
+	case "*":
+		//
+	case "/":
+		//
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
