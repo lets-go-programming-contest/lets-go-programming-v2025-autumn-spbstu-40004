92b671c1e00602cc264ad415bebb5312ddea9b05
cherkasoov <cherkasov5509@gmail.com>
2025-10-08 18:23:06 +0300

    [TASK-1] Input data processing

---
+++ nikita.cherkasov/task-1/cmd/service/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var operand1, operand2 int
+	_, err := fmt.Scan(&operand1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&operand2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	var operator string
+	_, err = fmt.Scan(&operator)
+	if err != nil {
+		fmt.Println("Invalid operator")
+		return
+	}
+}
