d3b3bdd9fec546854ab9c139bfa2c88642e1ced7
MrMels625 <MrMels625@gmail.com>
2025-10-09 19:03:05 +0300

    [TASK-2-1] input for N departments and input checks corrections

--- igor.aleksandrov/task-2-1/cmd/service/main.go
+++ igor.aleksandrov/task-2-1/cmd/service/main.go
@@ -5,15 +5,17 @@ import "fmt"
 func main() {
 	var departmentsCount int
 	_, err := fmt.Scanln(&departmentsCount)
-	if err != nil || departmentsCount < 0 {
+	if err != nil || departmentsCount < 1 || departmentsCount > 1000 {
 		fmt.Println("Invalid departments count!")
 		return
 	}
 
-	var employeesCount int
-	_, err = fmt.Scanln(&employeesCount)
-	if err != nil || employeesCount < 0 {
-		fmt.Println("Invalid employees count!")
-		return
+	for range departmentsCount {
+		var employeesCount int
+		_, err = fmt.Scanln(&employeesCount)
+		if err != nil || employeesCount < 1 || employeesCount > 1000 {
+			fmt.Println("Invalid employees count!")
+			return
+		}
 	}
 }
