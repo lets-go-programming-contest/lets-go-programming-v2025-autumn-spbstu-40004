5c7cd8b12f8286e13e2ff29d2f35c26822408efb
widgeiw <crazynas228@gmail.com>
2025-10-09 19:49:20 +0300

    [TASK-2-2] First attempt

--- orlova.alena/task-2-2/cmd/service/main.go
+++ orlova.alena/task-2-2/cmd/service/main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
+
+	intheap "task-2-2/cmd/service/internal/intheap"
 )
 
 func main() {
-	fmt.Println("TASK-2-2")
+	var (
+		numOfDishes   int
+		rate          int
+		numOfPrefDish int
+		myHeap        intheap.IntHeap
+	)
+
+	_, err := fmt.Scanln(&numOfDishes)
+	if err != nil {
+		fmt.Println("Wrong input")
+
+		return
+	}
+
+	heap.Init(&myHeap)
+
+	for range numOfDishes {
+		_, err = fmt.Scanln(&rate)
+		if err != nil {
+			fmt.Println("Wrong input")
+
+			return
+		}
+		heap.Push(&myHeap, rate)
+	}
+
+	_, err = fmt.Scanln(&numOfPrefDish)
+	if err != nil || numOfPrefDish > myHeap.Len() {
+		fmt.Println("Wrong input")
+
+		return
+	}
+
+	for range numOfPrefDish - 1 {
+		heap.Pop(&myHeap)
+	}
+
+	fmt.Println(heap.Pop(&myHeap))
 }
