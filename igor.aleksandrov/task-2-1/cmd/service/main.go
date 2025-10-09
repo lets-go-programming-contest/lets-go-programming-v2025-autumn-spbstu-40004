a99245c9450e415053c6fa6d12762786167056f6
MrMels625 <MrMels625@gmail.com>
2025-10-09 18:40:02 +0300

    [TASK-2-1] input count of departments and employees with check
    [TASK-2-1] check of negative values as well

--- igor.aleksandrov/task-2-1/cmd/service/main.go
+++ igor.aleksandrov/task-2-1/cmd/service/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
-}
+import "fmt"
 
+func main() {
+	var departmentsCount int
+	_, err := fmt.Scanln(&departmentsCount)
+	if err != nil || departmentsCount < 0 {
+		fmt.Println("Invalid departments count!")
+		return
+	}
+
+	var employeesCount int
+	_, err = fmt.Scanln(&employeesCount)
+	if err != nil || employeesCount < 0 {
+		fmt.Println("Invalid employees count!")
+		return
+	}
+}
