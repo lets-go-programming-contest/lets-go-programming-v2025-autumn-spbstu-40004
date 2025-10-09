5f68e864d16a383c7e78c55ba9ccbd4e8cdd1061
shycoshy <neforscist@gmail.com>
2025-10-09 19:28:41 +0300

    [TASK-1] initial commit

---
+++ asafov.andrew/task-1/cmd/service/main.go
@@ -0,0 +1,50 @@
+package main
+
+import ("fmt")
+
+func main()
+{
+	var a, b int
+
+	_, err := fmt.Scan(&a)
+	if err != nil
+	{
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err := fmt.Scan(&b)
+	if err != nil
+	{
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	var ch string
+
+	_, err := fmt.Scan(&ch)
+	if err != nil
+	{
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch operator
+	{
+		case "+":
+			fmt.Println(a + b)
+		case "-":
+			fmt.Println(a - b)
+		case "*":
+			fmt.Println(a * b)
+		case "/":
+			if b == 0
+			{
+				dmt.Println("Division by zero")
+				return
+			}
+			fmt.Println(a / b)
+		default:
+			fmt.Println("Invalid operation")
+	}
+}
