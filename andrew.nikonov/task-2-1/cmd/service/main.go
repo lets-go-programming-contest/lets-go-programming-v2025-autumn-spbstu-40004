d96bb7edf04deaecd428e7008fe3e255696e5649
ysffmn <andrewnikonov387@gmail.com>
2025-10-09 23:33:47 +0300

    [TASK-2-1] fixed linter errors: created new func for department processing and done some formating

--- andrew.nikonov/task-2-1/cmd/service/main.go
+++ andrew.nikonov/task-2-1/cmd/service/main.go
@@ -7,55 +7,62 @@ const (
 	maxTemp = 30
 )
 
+func processDep(emplQuantity int) {
+	var (
+		minDepTemp = minTemp
+		maxDepTemp = maxTemp
+	)
+
+	for range emplQuantity {
+		var (
+			operator string
+			temp     int
+		)
+
+		_, err := fmt.Scanln(&operator, &temp)
+		if err != nil || temp < minTemp || temp > maxTemp {
+			fmt.Println("Invalid temperature data")
+
+			return
+		}
+
+		switch operator {
+		case "<=":
+			maxDepTemp = min(maxDepTemp, temp)
+		case ">=":
+			minDepTemp = max(minDepTemp, temp)
+		default:
+			fmt.Println("Invalid operator")
+
+			return
+		}
+
+		if minDepTemp > maxDepTemp {
+			fmt.Println("-1")
+		} else {
+			fmt.Println(minDepTemp)
+		}
+	}
+}
+
 func main() {
-	var depQuantity int
+	var depQuantity, emplQuantity int
 
 	_, err := fmt.Scanln(&depQuantity)
 	if err != nil || depQuantity == 0 || depQuantity > 1000 {
 		fmt.Println("Invalid departments number")
+
 		return
 	}
 
-	for i := 0; i < depQuantity; i++ {
-		var (
-			emplQuantity int
-			minDepTemp   = minTemp
-			maxDepTemp   = maxTemp
-		)
-
-		_, err = fmt.Scanln(&emplQuantity)
+	for range depQuantity {
+		_, err := fmt.Scanln(&emplQuantity)
 		if err != nil || emplQuantity == 0 || emplQuantity > 1000 {
 			fmt.Println("Invalid employee number")
+
 			return
 		}
-
-		for j := 0; j < emplQuantity; j++ {
-			var (
-				operator string
-				temp     int
-			)
-
-			_, err := fmt.Scanln(&operator, &temp)
-			if err != nil || temp < minTemp || temp > maxTemp {
-				fmt.Println("Invalid temperature data")
-				return
-			}
-
-			switch operator {
-			case "<=":
-				maxDepTemp = min(maxDepTemp, temp)
-			case ">=":
-				minDepTemp = max(minDepTemp, temp)
-			default:
-				fmt.Println("Invalid operator")
-				return
-			}
-
-			if minDepTemp > maxDepTemp {
-				fmt.Println("-1")
-				return
-			}
-			fmt.Println(minDepTemp)
-		}
+		processDep(emplQuantity)
 	}
+
 }
