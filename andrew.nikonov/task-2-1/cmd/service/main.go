771aca8e74718b732126e851549accd25c11a054
ysffmn <andrewnikonov387@gmail.com>
2025-10-09 23:02:05 +0300

    [TASK-2-1] main logic implemented

--- andrew.nikonov/task-2-1/cmd/service/main.go
+++ andrew.nikonov/task-2-1/cmd/service/main.go
@@ -1,4 +1,61 @@
 package main
 
+import "fmt"
+
+const (
+	minTemp = 15
+	maxTemp = 30
+)
+
 func main() {
+	var depQuantity int
+
+	_, err := fmt.Scanln(&depQuantity)
+	if err != nil || depQuantity == 0 || depQuantity > 1000 {
+		fmt.Println("Invalid departments number")
+		return
+	}
+
+	for i := 0; i < depQuantity; i++ {
+		var (
+			emplQuantity int
+			minDepTemp   = minTemp
+			maxDepTemp   = maxTemp
+		)
+
+		_, err = fmt.Scanln(&emplQuantity)
+		if err != nil || emplQuantity == 0 || emplQuantity > 1000 {
+			fmt.Println("Invalid employee number")
+			return
+		}
+
+		for j := 0; j < emplQuantity; j++ {
+			var (
+				operator string
+				temp     int
+			)
+
+			_, err := fmt.Scanln(&operator, &temp)
+			if err != nil || temp < minTemp || temp > maxTemp {
+				fmt.Println("Invalid temperature data")
+				return
+			}
+
+			switch operator {
+			case "<=":
+				maxDepTemp = min(maxDepTemp, temp)
+			case ">=":
+				minDepTemp = max(minDepTemp, temp)
+			default:
+				fmt.Println("Invalid operator")
+				return
+			}
+
+			if minDepTemp > maxDepTemp {
+				fmt.Println("-1")
+				return
+			}
+			fmt.Println(minDepTemp)
+		}
+	}
 }
