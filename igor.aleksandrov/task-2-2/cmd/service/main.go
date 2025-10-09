976d6889cb3207da11a5bbf74cd047da5b2d0c99
MrMels625 <MrMels625@gmail.com>
2025-10-09 20:17:19 +0300

    [TASK-2-2] input dishes count with check added

--- igor.aleksandrov/task-2-2/cmd/service/main.go
+++ igor.aleksandrov/task-2-2/cmd/service/main.go
@@ -1,4 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
+	var dishesCount int
+
+	_, err := fmt.Scanln(&dishesCount)
+	if err != nil || dishesCount < 1 || dishesCount > 10000 {
+		fmt.Println("Invalid dishes count!")
+
+		return
+	}
 }
