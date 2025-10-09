3811971df4346fe5b390cf4440beb692612640a5
ysffmn <andrewnikonov387@gmail.com>
2025-10-09 20:10:48 +0300

    [TASK-1] logic simplification and var united

--- andrew.nikonov/task-1/cmd/service/main.go
+++ andrew.nikonov/task-1/cmd/service/main.go
@@ -3,16 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var operand1, operand2 int
-	var operator string
-	cnt, err := fmt.Scan(&operand1, &operand2, &operator)
+	var (
+		operand1 int
+		operand2 int
+		operator string
+	)
+	_, err := fmt.Scanln(&operand1)
 	if err != nil {
-		switch cnt {
-		case 0:
-			fmt.Println("Invalid first operand")
-		case 1:
-			fmt.Println("Invalid second operand")
-		}
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scanln(&operand2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scanln(&operator)
+	if err != nil {
+		fmt.Println("Invalid input for operator")
 		return
 	}
 	switch operator {
