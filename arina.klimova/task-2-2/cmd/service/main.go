ac747dcaed6000bca98d610d1124310a729448b0
arinaklimova <arina.klim.06@mail.ru>
2025-10-09 21:20:28 +0300

    [TASK-2-2] add basic MaxHeap type and interface methods

--- arina.klimova/task-2-2/cmd/service/main.go
+++ arina.klimova/task-2-2/cmd/service/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 )
+
+type MaxHeap []int
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
 
 func main() {
 	var n int
 	fmt.Scanln(&n)
+
+	h := &MaxHeap{}
+	heap.Init(h)
 
 	var k int
 	fmt.Scan(&k)
