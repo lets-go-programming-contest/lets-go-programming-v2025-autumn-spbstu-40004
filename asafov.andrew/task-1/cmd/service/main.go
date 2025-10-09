d010775eaa64e016876d716605846473234ca8b8
shycoshy <neforscist@gmail.com>
2025-10-09 20:06:53 +0300

    [TASK-1] compilation fix

--- asafov.andrew/task-1/cmd/service/main.go
+++ asafov.andrew/task-1/cmd/service/main.go
@@ -2,34 +2,30 @@ package main
 
 import ("fmt")
 
-func main()
-{
+func main() {
 	var a, b int
 
 	_, err := fmt.Scan(&a)
-	if err != nil
-	{
+	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
 
-	_, err := fmt.Scan(&b)
-	if err != nil
-	{
+	_, err = fmt.Scan(&b)
+	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
 
 	var ch string
 
-	_, err := fmt.Scan(&ch)
-	if err != nil
-	{
+	_, err = fmt.Scan(&ch)
+	if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
 
-	switch operator
+	switch ch
 	{
 		case "+":
 			fmt.Println(a + b)
@@ -38,9 +34,8 @@ func main()
 		case "*":
 			fmt.Println(a * b)
 		case "/":
-			if b == 0
-			{
-				dmt.Println("Division by zero")
+			if b == 0 {
+				fmt.Println("Division by zero")
 				return
 			}
 			fmt.Println(a / b)
