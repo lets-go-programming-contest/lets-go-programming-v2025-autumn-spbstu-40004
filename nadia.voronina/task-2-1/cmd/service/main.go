3f93a3f1e8e4e6352d6441304e4cc27b47a472de
Voronina <nadia.v.voronina@gmail.com>
2025-10-10 23:15:52 +0300

    [TASK-2-1] Fixing link

--- nadia.voronina/task-2-1/cmd/service/main.go
+++ nadia.voronina/task-2-1/cmd/service/main.go
@@ -5,58 +5,67 @@ import (
 )
 
 func main() {
-	var n, k int
+	var numberOfDepartments, numberOfEmployees int
 
-	_, errN := fmt.Scanln(&n)
+	_, errN := fmt.Scanln(&numberOfDepartments)
 	if errN != nil {
 		fmt.Println("Invalid number of departments")
 		return
 	}
 
-	for i := 0; i < n; i++ {
+	for range numberOfDepartments {
 
-		_, errK := fmt.Scanln(&k)
+		_, errK := fmt.Scanln(&numberOfEmployees)
 		if errK != nil {
 			fmt.Println("Invalid number of employees")
+
 			return
 		}
 
-		var maxDegree int = 31
-		var minDegree int = 14
+		maxDegree := 31
+		minDegree := 14
 
-		for j := 0; j < k; j++ {
+		for range numberOfEmployees {
+
 			var sign string
+
 			var degree int
 
 			_, err1 := fmt.Scan(&sign)
 			if err1 != nil {
 				fmt.Println("Invalid sign")
+
 				return
 			}
 
 			_, err2 := fmt.Scanln(&degree)
 			if err2 != nil {
 				fmt.Println("Invalid degree")
+
 				return
 			}
-			if sign == "<=" {
+			switch sign {
+			case "<=":
 				if (maxDegree >= degree) && (minDegree <= degree) {
 					maxDegree = degree
+
 					fmt.Println(minDegree)
 				} else if (maxDegree <= degree) && (minDegree <= degree) {
 					fmt.Println(minDegree)
 				} else {
 					fmt.Println(-1)
 				}
-			} else if sign == ">=" {
+			case ">=":
 				if (minDegree <= degree) && (maxDegree >= degree) {
 					minDegree = degree
+
 					fmt.Println(minDegree)
 				} else {
 					fmt.Println(-1)
 				}
-			} else {
+			default:
 				fmt.Println("Wrong sign has been added")
+
 				return
 			}
 		}
