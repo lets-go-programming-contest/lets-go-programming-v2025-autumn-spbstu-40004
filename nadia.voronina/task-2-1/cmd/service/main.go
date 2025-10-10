bbe4485f0bfcc6ac632ecc67fc0e8fb1a84a15fb
Voronina <nadia.v.voronina@gmail.com>
2025-10-11 00:54:37 +0300

    [TASK-2-1] Fixing lint main separated

--- nadia.voronina/task-2-1/cmd/service/main.go
+++ nadia.voronina/task-2-1/cmd/service/main.go
@@ -4,6 +4,32 @@ import (
 	"fmt"
 )
 
+func processCondition(maxDegree *int, minDegree *int, degree int, sign string) {
+	switch sign {
+	case "<=":
+		if (*maxDegree >= degree) && (*minDegree <= degree) {
+			*maxDegree = degree
+
+			fmt.Println(*minDegree)
+		} else if (*maxDegree <= degree) && (*minDegree <= degree) {
+			fmt.Println(*minDegree)
+		} else {
+			fmt.Println(-1)
+		}
+	case ">=":
+		if (*minDegree <= degree) && (*maxDegree >= degree) {
+			*minDegree = degree
+
+			fmt.Println(*minDegree)
+		} else {
+			fmt.Println(-1)
+		}
+	default:
+		fmt.Println("Wrong sign has been added")
+
+		return
+	}
+}
 func main() {
 	var numberOfDepartments, numberOfEmployees int
 
@@ -43,32 +69,7 @@ func main() {
 
 				return
 			}
-
-			switch sign {
-			case "<=":
-				switch {
-				case (maxDegree >= degree) && (minDegree <= degree):
-					maxDegree = degree
-
-					fmt.Println(minDegree)
-				case (maxDegree <= degree) && (minDegree <= degree):
-					fmt.Println(minDegree)
-				default:
-					fmt.Println(-1)
-				}
-			case ">=":
-				if (minDegree <= degree) && (maxDegree >= degree) {
-					minDegree = degree
-
-					fmt.Println(minDegree)
-				} else {
-					fmt.Println(-1)
-				}
-			default:
-				fmt.Println("Wrong sign has been added")
-
-				return
-			}
+			processCondition(&maxDegree, &minDegree, degree, sign)
 		}
 	}
 }
