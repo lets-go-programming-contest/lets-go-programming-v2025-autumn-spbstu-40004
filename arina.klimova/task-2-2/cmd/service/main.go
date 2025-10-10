47d879dce695c5321ce718c0c1892e6f61581fa1
arinaklimova <arina.klim.06@mail.ru>
2025-10-10 16:49:41 +0300

    [TASK-2-2] add new package

--- arina.klimova/task-2-2/cmd/service/main.go
+++ arina.klimova/task-2-2/cmd/service/main.go
@@ -3,33 +3,9 @@ package main
 import (
 	"container/heap"
 	"fmt"
+
+	maxheap "github.com/arinaklimova/task-2-2/internal/maxheap"
 )
-
-type MaxHeap []int
-
-func (h *MaxHeap) Len() int           { return len(*h) }
-func (h *MaxHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
-func (h *MaxHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
-
-func (h *MaxHeap) Push(x interface{}) {
-	val, ok := x.(int)
-	if !ok {
-		fmt.Println("Type assertion failed")
-
-		return
-	}
-
-	*h = append(*h, val)
-}
-
-func (h *MaxHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[:n-1]
-
-	return x
-}
 
 func main() {
 	var numberOfDishes int
@@ -41,7 +17,7 @@ func main() {
 		return
 	}
 
-	myHeap := &MaxHeap{}
+	myHeap := &maxheap.MaxHeap{}
 	heap.Init(myHeap)
 
 	for range numberOfDishes {
