ed3e59c7620618599d422e06ad40e259d8adc793
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2025-10-11 13:22:46 +0300

    [TASK-3] Refined YAML reading

--- veniamin.belyaev/task-3/cmd/service/main.go
+++ veniamin.belyaev/task-3/cmd/service/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"flag"
+	"strings"
+
+	config_handler "github.com/belyaevEDU/task-3/internal"
+)
+
 func main() {
+	var configFilePath string
+
+	flag.StringVar(&configFilePath, "config", "none", "Configuration file path")
+	flag.Parse()
+
+	if strings.Compare(configFilePath, "") == 0 {
+		panic("config file path via flag not specified")
+	}
+
+	config, err := config_handler.LoadConfig(configFilePath)
+	if err != nil {
+		panic(err.Error())
+	}
 
 }
