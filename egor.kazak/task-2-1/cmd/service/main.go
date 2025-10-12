2b91ed4458a4e668ac9386609d9e4c921cf609a8
Cuathimbong <egvika20051@gmail.com>
2025-10-12 14:50:06 +0300

    [TASK-2-1] Lint fix number 4

--- egor.kazak/task-2-1/cmd/service/main.go
+++ egor.kazak/task-2-1/cmd/service/main.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-  "fmt"
-  "log"
+	"fmt"
+	"log"
 )
 
 func main() {
-  var count, constraints int
+	var count, constraints int
 
-  _, err := fmt.Scan(&count, &constraints)
-  if err != nil {
-    log.Fatal(err)
-  }
+	_, err := fmt.Scan(&count, &constraints)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  for i := 0; i < count; i++ {
-    const minTemp = 15
-    currentMin := minTemp
+	for i := 0; i < count; i++ {
+		const minTemp = 15
+		currentMin := minTemp
 
-    const maxTemp = 30
-    currentMax := maxTemp
+		const maxTemp = 30
+		currentMax := maxTemp
 
-    for j := 0; j < constraints; j++ {
-      var operator string
-      var temperature int
+		for j := 0; j < constraints; j++ {
+			var operator string
+			var temperature int
 
-      _, err := fmt.Scan(&operator, &temperature)
-      if err != nil {
-        log.Fatal(err)
-      }
+			_, err := fmt.Scan(&operator, &temperature)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-      switch operator {
-      case ">=":
-        if temperature > currentMin {
-          currentMin = temperature
-        }
-      case "<=":
-        if temperature < currentMax {
-          currentMax = temperature
-        }
-      }
+			switch operator {
+			case ">=":
+				if temperature > currentMin {
+					currentMin = temperature
+				}
+			case "<=":
+				if temperature < currentMax {
+					currentMax = temperature
+				}
+			}
 
-      if currentMin <= currentMax {
-        fmt.Println(currentMin)
-      } else {
-        fmt.Println(-1)
-      }
-    }
-  }
+			if currentMin <= currentMax {
+				fmt.Println(currentMin)
+			} else {
+				fmt.Println(-1)
+			}
+		}
+	}
 }
