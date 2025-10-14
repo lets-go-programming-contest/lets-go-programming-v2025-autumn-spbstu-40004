460d73fd7c92642f59ecfb1879ebdb5fed7ed857
ysffmn <andrewnikonov387@gmail.com>
2025-10-15 01:43:29 +0300

    [TASK-2-2] main skeleton

--- andrew.nikonov/task-2-2/cmd/service/main.go
+++ andrew.nikonov/task-2-2/cmd/service/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+var errInput = errors.New("incorrect input")
+
 func main() {
+	var dishesNum int
+
+	_, err := fmt.Scanln(&dishesNum)
+	if err != nil || dishesNum < -10000 || dishesNum > 10000 {
+		fmt.Println(errInput)
+
+		return
+	}
+
+	//TO DO: reading and processing heap
+
+	var preferredNum int
+
+	_, err = fmt.Scanln(&preferredNum)
+	if err != nil || preferredNum < 1 || preferredNum > dishesNum {
+		fmt.Println(errInput)
+
+		return
+	}
 
 }
