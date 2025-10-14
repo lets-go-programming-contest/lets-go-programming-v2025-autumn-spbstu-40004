e871dfb0a763f64761eda3107199444086d0206c
ysffmn <andrewnikonov387@gmail.com>
2025-10-15 01:02:27 +0300

    [TASK-2-2] added IntMaxHeap interface

---
+++ andrew.nikonov/task-2-2/internal/intMaxHeap/intMaxHeap.go
@@ -0,0 +1,36 @@
+package intmaxheap
+
+type IntMaxHeap []int
+
+func (h *IntMaxHeap) Len() int {
+	return len(*h)
+}
+
+func (h *IntMaxHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
+}
+
+func (h *IntMaxHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *IntMaxHeap) Push(val any) {
+	intVal, isCorrect := val.(int)
+	if isCorrect {
+		*h = append(*h, intVal)
+	}
+}
+
+func (h *IntMaxHeap) Pop() any {
+	prev := *h
+	prevLen := len(prev)
+
+	if prevLen == 0 {
+		return nil
+	}
+
+	el := prev[prevLen-1]
+	*h = prev[:prevLen-1]
+
+	return el
+}
