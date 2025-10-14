ed1c54089ae844c91e30131081d6a9ac025b470d
ysffmn <andrewnikonov387@gmail.com>
2025-10-15 02:03:14 +0300

    [TASK-2-2] implemented input and main logic

--- andrew.nikonov/task-2-2/cmd/service/main.go
+++ andrew.nikonov/task-2-2/cmd/service/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"container/heap"
 	"errors"
 	"fmt"
+
+	"github.com/ysffmn/task-2-2/internal/intmaxheap"
 )
 
 var errInput = errors.New("incorrect input")
@@ -17,7 +20,21 @@ func main() {
 		return
 	}
 
-	//TO DO: reading and processing heap
+	dishesHeap := &intmaxheap.IntMaxHeap{}
+	heap.Init(dishesHeap)
+
+	for range dishesNum {
+		var rating int
+
+		_, err = fmt.Scan(&rating)
+		if err != nil || rating < -10000 || rating > 10000 {
+			fmt.Println(errInput)
+
+			return
+		}
+
+		heap.Push(dishesHeap, rating)
+	}
 
 	var preferredNum int
 
@@ -28,4 +45,9 @@ func main() {
 		return
 	}
 
+	for range preferredNum - 1 {
+		dishesHeap.Pop()
+	}
+
+	fmt.Println(dishesHeap.Pop())
 }
