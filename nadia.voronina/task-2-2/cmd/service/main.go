ec4c8f599f5c2e402a3b3ef9cab54b5b12445ed6
Voronina <nadia.v.voronina@gmail.com>
2025-10-15 22:45:53 +0300

    [TASK-2-2] Fixing lint

--- nadia.voronina/task-2-2/cmd/service/main.go
+++ nadia.voronina/task-2-2/cmd/service/main.go
@@ -7,11 +7,12 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(x interface{}) {
+	_ = x.(int)
 	*h = append(*h, x.(int))
 }
 
@@ -34,12 +35,14 @@ func main() {
 	}
 
 	var dishes IntHeap
+
 	heap.Init(&dishes)
+
 	var dish int
 
 	for range numberOfDishes {
-
 		_, errK := fmt.Scan(&dish)
+
 		if errK != nil {
 			fmt.Println("Invalid dish")
 
@@ -50,7 +53,9 @@ func main() {
 	}
 
 	var preferredDish int
+
 	_, errK := fmt.Scan(&preferredDish)
+
 	if errK != nil {
 		fmt.Println("Invalid preferred dish")
 
@@ -60,5 +65,6 @@ func main() {
 	for range numberOfDishes - preferredDish {
 		heap.Pop(&dishes)
 	}
+
 	fmt.Printf("%d.", heap.Pop(&dishes))
 }
