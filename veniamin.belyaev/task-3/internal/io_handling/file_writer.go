1951f0866f3447928285187abc1529f0e9505a71
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2025-10-16 18:18:24 +0300

    [TASK-3] Restructuring internal files

--- veniamin.belyaev/task-3/internal/io_handling/file_writer.go
+++ veniamin.belyaev/task-3/internal/io_handling/file_writer.go
@@ -4,14 +4,14 @@ import (
 	"os"
 )
 
-func WriteStringToFile(filename string, data string) error {
+func WriteStringToFile(filename string, data []byte) error {
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	_, err = file.WriteString(data)
+	_, err = file.Write(data)
 
 	return err
 }
