c3679289c04399e30fa764f225bc7abed321dfd7
jambii1 <s.maslevtsov@gmail.com>
2025-10-16 19:51:44 +0300

    [TASK-3] Fix currency tags alignment, declaration cuddling and error check with switch

--- stanislav.maslevtsov/task-3/internal/currenciesprocessing/currency.go
+++ stanislav.maslevtsov/task-3/internal/currenciesprocessing/currency.go
@@ -1,9 +1,9 @@
 package currenciesprocessing
 
 type Currency struct {
-	NumCode  int    `xml:"NumCode" json:"num_code"`
+	NumCode  int    `xml:"NumCode"  json:"num_code"`
 	CharCode string `xml:"CharCode" json:"char_code"`
-	Value    string `xml:"Value" json:"value"`
+	Value    string `xml:"Value"    json:"value"`
 }
 
 type Currencies struct {
