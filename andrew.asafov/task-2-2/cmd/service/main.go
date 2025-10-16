adf7f47bb3404c4eb7a295f234572d6528b44bdf
shycoshy <neforscist@gmail.com>
2025-10-17 01:55:13 +0300

    [TASK-2-2] fix no.5

--- andrew.asafov/task-2-2/cmd/service/main.go
+++ andrew.asafov/task-2-2/cmd/service/main.go
@@ -7,15 +7,16 @@ import (
 
 type MaxHeap []int
 
-func (h MaxHeap) Len() int           { return len(h) }
-func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
-func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *MaxHeap) Len() int           { return len(*h) }
+func (h *MaxHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *MaxHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *MaxHeap) Push(x interface{}) {
 	value, ok := x.(int)
 	if !ok {
 		return
 	}
+
 	*h = append(*h, value)
 }
 
@@ -23,6 +24,7 @@ func (h *MaxHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
+
 	*h = old[0 : n-1]
 
 	return x
