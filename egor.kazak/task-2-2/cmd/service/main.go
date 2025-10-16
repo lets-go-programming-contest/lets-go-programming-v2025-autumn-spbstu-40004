7e5da39013c29b2b1a39679da490c6109edfa831
Cuathimbong <egvika20051@gmail.com>
2025-10-16 19:32:42 +0300

    [TASK-2-2] Some interesting fixes

--- egor.kazak/task-2-2/cmd/service/main.go
+++ egor.kazak/task-2-2/cmd/service/main.go
@@ -3,23 +3,24 @@ package main
 import (
 	"container/heap"
 	"fmt"
-	"log"
 )
 
 type IntHeap []int
 
-func (h IntHeap) Len() int { return len(h) }
+func (h IntHeap) Len() int {
+	return len(h)
+}
 
-func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
 
-func (h IntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
 
 func (h *IntHeap) Push(x interface{}) {
-	num, ok := x.(int)
-	if !ok {
-		log.Fatal("type assertion to int failed")
-	}
-	*h = append(*h, num)
+	*h = append(*h, x.(int))
 }
 
 func (h *IntHeap) Pop() interface{} {
@@ -31,40 +32,26 @@ func (h *IntHeap) Pop() interface{} {
 }
 
 func main() {
-	var dishCount, preferenceOrder int
-	_, err := fmt.Scan(&dishCount)
-	if err != nil {
-		log.Fatal(err)
+	var n, k int
+	fmt.Scan(&n)
+
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&arr[i])
 	}
 
-	ratings := make([]int, dishCount)
-	for index := range ratings {
-		_, err := fmt.Scan(&ratings[index])
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	fmt.Scan(&k)
 
-	_, err = fmt.Scan(&preferenceOrder)
-	if err != nil {
-		log.Fatal(err)
-	}
+	h := &IntHeap{}
+	heap.Init(h)
 
-	heapInstance := &IntHeap{}
-	heap.Init(heapInstance)
-
-	for _, rating := range ratings {
-		heap.Push(heapInstance, rating)
+	for i := 0; i < n; i++ {
+		heap.Push(h, arr[i])
 	}
 
 	var result int
-	for range preferenceOrder {
-		item := heap.Pop(heapInstance)
-		num, ok := item.(int)
-		if !ok {
-			log.Fatal("type assertion to int failed")
-		}
-		result = num
+	for i := 0; i < k; i++ {
+		result = heap.Pop(h).(int)
 	}
 
 	fmt.Println(result)
