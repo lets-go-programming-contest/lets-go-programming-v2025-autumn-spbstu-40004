0c742d97575b9c79548326afe65d2dbffa4ed285
shycoshy <neforscist@gmail.com>
2025-10-17 01:27:14 +0300

    [TASK-2-2] fix no.1

--- andrew.asafov/task-2-2/cmd/service/main.go
+++ andrew.asafov/task-2-2/cmd/service/main.go
@@ -1,23 +1,59 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
-	"sort"
 )
 
-func main() {
-	var n, k int
-	fmt.Scan(&n)
+type MaxHeap []int
 
-	arr := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&arr[i])
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func main() {
+	var dishCount, kthPreference int
+	_, scanErr := fmt.Scan(&dishCount)
+	if scanErr != nil {
+		return
 	}
 
-	fmt.Scan(&k)
+	dishRatings := make([]int, dishCount)
+	for index := range dishCount {
+		_, scanErr = fmt.Scan(&dishRatings[index])
+		if scanErr != nil {
+			return
+		}
+	}
 
-	sort.Sort(sort.Reverse(sort.IntSlice(arr)))
+	_, scanErr = fmt.Scan(&kthPreference)
+	if scanErr != nil {
+		return
+	}
 
-	result := arr[k-1]
+	maxHeap := &MaxHeap{}
+	heap.Init(maxHeap)
+
+	for _, rating := range dishRatings {
+		heap.Push(maxHeap, rating)
+	}
+
+	var result int
+	for range kthPreference {
+		result = heap.Pop(maxHeap).(int)
+	}
+
 	fmt.Println(result)
 }
