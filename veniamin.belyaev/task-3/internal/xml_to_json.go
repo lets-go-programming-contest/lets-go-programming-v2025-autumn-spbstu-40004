e005800ecf381a177961f0730f95c1a7ded66006
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2025-10-16 17:44:41 +0300

    [TASK-3] Added XML to JSON structures convertion

---
+++ veniamin.belyaev/task-3/internal/xml_to_json.go
@@ -0,0 +1,19 @@
+package internal
+
+type CurrencyJSON struct {
+	NumericalCode int     `json:"num_code"`
+	CharacterCode string  `json:"char_code"`
+	Value         float32 `json:"value"`
+}
+
+func ConvertXMLStructsToJson(currenciesXML CurrenciesXML) []CurrencyJSON {
+	arrayLength := len(currenciesXML.Currencies)
+
+	var currenciesJSON = make([]CurrencyJSON, arrayLength)
+
+	for index := 0; index < arrayLength; index++ {
+		currenciesJSON[index] = CurrencyJSON(currenciesXML.Currencies[index])
+	}
+
+	return currenciesJSON
+}
