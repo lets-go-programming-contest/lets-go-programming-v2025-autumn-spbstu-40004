e305dc5bec92977bc3a8dda7c1e775d60c0ed121
qyantor <ilyxaplay220@gmail.com>
2025-10-17 17:16:03 +0300

    [TASK-2-1] fixed lint

--- ilya.ageev/task-2-1/cmd/service/main.go
+++ ilya.ageev/task-2-1/cmd/service/main.go
@@ -8,42 +8,45 @@ import (
 
 func main() {
 	var (
-		n, k, T int
-		str     string
+		numOfDepart, numOfWork, Temp int
+		str                          string
 	)
 	results := make([]string, 0)
 
-	_, err := fmt.Scan(&n)
+	_, err := fmt.Scan(&numOfDepart)
 	if err != nil {
 		fmt.Println("Invalid number of departments")
 		return
 	}
 
-	for i := 0; i < n; i++ {
-		_, err := fmt.Scan(&k)
+	for i := 0; i < numOfDepart; i++ {
+		_, err := fmt.Scan(&numOfWork)
 		if err != nil {
 			fmt.Println("invalid number of workers")
 			return
 		}
-
+		const (
+			minAllowedTemp = 15
+			maxAllowedTemp = 30
+		)
 		minT := 15
 		maxT := 30
 		departmentResults := make([]string, 0)
 
-		for j := 0; j < k; j++ {
-			_, err := fmt.Scan(&str, &T)
-			if err != nil {
+		for j := 0; j < numOfWork; j++ {
+			_, err := fmt.Scan(&str, &Temp)
+			if err != nil || Temp > maxAllowedTemp || Temp < minAllowedTemp {
 				fmt.Println("Invalid temperature")
 				return
 			}
 
 			if str == ">=" {
-				if T > minT {
-					minT = T
+				if Temp > minT {
+					minT = Temp
 				}
 			} else if str == "<=" {
-				if T < maxT {
-					maxT = T
+				if Temp < maxT {
+					maxT = Temp
 				}
 			}
 
