07546b4c60aab525586ffb0103990e99f4ae68a2
danil@debian1337 <un228der@gmail.com>
2025-10-17 21:55:58 +0300

    [TASK-2-1] Implementation of task

--- kushekbaev.danil/task-2-1/cmd/service/main.go
+++ kushekbaev.danil/task-2-1/cmd/service/main.go
@@ -1,3 +1,73 @@
+import (
+	"fmt"
+)
+
+const (
+	minTemp = 15
+	maxTemp = 30
+	invalidValue = -1
+)
+
 func main{
-	
+	var (
+		departmentNumber int
+		employeeNumber   int
+		desireableTemp   int
+		operator         string
+	)
+
+	_, err := fmt.Scan(&departmentNumber)
+	if err != nil {
+		fmt.Println("Error while reading number of departments")
+
+		return
+	}
+
+	for range departmentNumber {
+		_, err = fmt.Scan(&employeeNumber)
+		if err != nil {
+			fmt.Println("Error while reading number of employees")
+
+			return
+		}
+
+		lowerBound := minTemp
+		upperBound := maxTemp
+
+		for range employeeNumber {
+			_, err = fmt.Scan(&operator)
+			if err != nil {
+				fmt.Println("Error while reading operator")
+
+				return
+			}
+
+			_, err = fmt.Scan(&desireableTemp)
+			if err != nil {
+				fmt.Println("Error while reading desireable temperature")
+
+				return
+			}
+
+			if desireableTemp < minTemp || desireableTemp > maxTemp {
+				fmt.Println(invalidValue)
+			}
+
+			switch operator {
+			case "<=":
+				upperBound = Ternary(upperBound < desireableTemp, upperBound, desireableTemp)
+			case ">=":
+				lowerBound = Ternary(lowerBound > desireableTemp, lowerBound, desireableTemp)
+			default:
+				fmt.Println(Invalid operator)
+			}
+
+			if (lowerBound <= upperBound) {
+				fmt.Println(lowerBound)
+			} else {
+				fmt.Println(invalidValue)
+			}
+		}
+	}
+
 }
