6f653cdac1fc4393f022c26d6ae3d596485bd66b
qyantor <ilyxaplay220@gmail.com>
2025-10-17 17:27:52 +0300

    [TASK-2-1] trying to fix lint

--- ilya.ageev/task-2-1/cmd/service/main.go
+++ ilya.ageev/task-2-1/cmd/service/main.go
@@ -11,32 +11,38 @@ func main() {
 		numOfDepart, numOfWork, Temp int
 		str                          string
 	)
-	results := make([]string, 0)
 
 	_, err := fmt.Scan(&numOfDepart)
 	if err != nil {
 		fmt.Println("Invalid number of departments")
+
 		return
 	}
 
-	for i := 0; i < numOfDepart; i++ {
+	results := make([]string, 0)
+
+	for range numOfDepart {
 		_, err := fmt.Scan(&numOfWork)
 		if err != nil {
 			fmt.Println("invalid number of workers")
+
 			return
 		}
+
 		const (
 			minAllowedTemp = 15
 			maxAllowedTemp = 30
 		)
+
 		minT := 15
 		maxT := 30
 		departmentResults := make([]string, 0)
 
-		for j := 0; j < numOfWork; j++ {
+		for range numOfWork {
 			_, err := fmt.Scan(&str, &Temp)
 			if err != nil || Temp > maxAllowedTemp || Temp < minAllowedTemp {
 				fmt.Println("Invalid temperature")
+
 				return
 			}
 
@@ -51,14 +57,12 @@ func main() {
 			}
 
 			if minT <= maxT {
-				// Use the maximum possible temperature that satisfies all constraints
 				departmentResults = append(departmentResults, strconv.Itoa(minT))
 			} else {
 				departmentResults = append(departmentResults, "-1")
 			}
 		}
 
-		// Add all results for this department to the main results
 		results = append(results, departmentResults...)
 	}
 
