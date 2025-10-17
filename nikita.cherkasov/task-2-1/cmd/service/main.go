a83b33a09bf1454bf26f3659888a4a97b5ba76eb
cherkasoov <cherkasov5509@gmail.com>
2025-10-17 21:13:42 +0300

    [TASK-2-1] Add func updateTemperatureRange

--- nikita.cherkasov/task-2-1/cmd/service/main.go
+++ nikita.cherkasov/task-2-1/cmd/service/main.go
@@ -1,2 +1,34 @@
 package main
 
+import (
+	"errors"
+)
+
+var errOperation = errors.New("Invalid operation")
+
+func updateTemperatureRange(minTemp int, maxTemp int, targetTemp int, operation string) (int, int, error) {
+	if minTemp == -1 && maxTemp == -1 {
+		return minTemp, maxTemp, nil
+	}
+
+	switch operation {
+	case ">=":
+		if targetTemp > maxTemp {
+			minTemp = -1
+			maxTemp = -1
+		} else if minTemp <= targetTemp && targetTemp <= maxTemp {
+			minTemp = targetTemp
+		}
+	case "<=":
+		if targetTemp < minTemp {
+			minTemp = -1
+			maxTemp = -1
+		} else if minTemp <= targetTemp && targetTemp <= maxTemp {
+			maxTemp = targetTemp
+		}
+	default:
+		return minTemp, maxTemp, errOperation
+	}
+
+	return minTemp, maxTemp, nil
+}
