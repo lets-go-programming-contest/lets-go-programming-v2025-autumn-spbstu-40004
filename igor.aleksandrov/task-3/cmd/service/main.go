a7cb86ed5eed64a66e339f7388fd7eccadda4ebe
MrMels625 <MrMels625@gmail.com>
2025-10-21 11:42:32 +0300

    [TASK-3] config structure and command line flags added

--- igor.aleksandrov/task-3/cmd/service/main.go
+++ igor.aleksandrov/task-3/cmd/service/main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
 func main() {
+	configPath := flag.String("config", "", "Path to the YAML config file")
+
+	flag.Parse()
+
+	if *configPath == "" {
+		fmt.Println("--config flag is required")
+
+		return
+	}
+
+	fmt.Printf("Config file path: %s\n", *configPath)
 }
