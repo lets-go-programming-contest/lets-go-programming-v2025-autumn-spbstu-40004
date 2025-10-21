0716337062f135a95617aea2d4ef66ef22ecad13
MrMels625 <MrMels625@gmail.com>
2025-10-21 11:56:36 +0300

    [TASK-3] errors handling, reading and unmarshaling config file added

--- igor.aleksandrov/task-3/cmd/service/main.go
+++ igor.aleksandrov/task-3/cmd/service/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
 )
 
 type Config struct {
@@ -21,5 +23,17 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Config file path: %s\n", *configPath)
+	data, err := os.ReadFile(*configPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to read config file %s: %w", *configPath, err))
+	}
+
+	var conf Config
+
+	err = yaml.Unmarshal(data, &conf)
+	if err != nil {
+		panic(fmt.Errorf("failed to unmarshal config file: %w", err))
+	}
+
+	fmt.Printf("Config loaded:\nInput: %s\nOutput: %s\n", conf.InputFile, conf.OutputFile)
 }
