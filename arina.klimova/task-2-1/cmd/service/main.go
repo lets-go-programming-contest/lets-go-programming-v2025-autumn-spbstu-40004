c30e33174f526767a15a0ffa01cbac9461877bef
arinaklimova <arina.klim.06@mail.ru>
2025-10-24 13:38:07 +0300

    [TASK-2-1] new package temperature

--- arina.klimova/task-2-1/cmd/service/main.go
+++ arina.klimova/task-2-1/cmd/service/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
-)
 
-const (
-	minimalTemp = 15
-	maximalTemp = 30
+	TempControl "github.com/arinaklimova/task-2-1/temperature"
 )
 
 var (
@@ -15,46 +12,7 @@ var (
 	ErrReadingPeople     = errors.New("error reading people count")
 	ErrReadingTemp       = errors.New("error reading operation or temperature")
 	ErrProcessingDept    = errors.New("error processing department")
-	ErrInvalidOperation  = errors.New("invalid operation")
-	ErrInvalidTempRange  = errors.New("temperature range is invalid")
 )
-
-type TemperatureController struct {
-	minTemp int
-	maxTemp int
-}
-
-func NewTemperatureController() *TemperatureController {
-	return &TemperatureController{
-		minTemp: minimalTemp,
-		maxTemp: maximalTemp,
-	}
-}
-
-func (tc *TemperatureController) UpdateTemperature(operation string, personTemp int) error {
-	switch operation {
-	case ">=":
-		if personTemp > tc.minTemp {
-			tc.minTemp = personTemp
-		}
-	case "<=":
-		if personTemp < tc.maxTemp {
-			tc.maxTemp = personTemp
-		}
-	default:
-		return ErrInvalidOperation
-	}
-
-	return nil
-}
-
-func (tc *TemperatureController) GetCurrentTemperature() (int, error) {
-	if tc.minTemp > tc.maxTemp {
-		return -1, ErrInvalidTempRange
-	}
-
-	return tc.minTemp, nil
-}
 
 func main() {
 	var departmentCount int
@@ -76,7 +34,7 @@ func main() {
 			return
 		}
 
-		tempControl := NewTemperatureController()
+		tempControl := &TempControl.NewTemperatureController()
 
 		for range peopleCount {
 			var (
