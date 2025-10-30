361eddca437a8aff3bde03c3a2e5dc54c96bdf2c
widgeiw <crazynas228@gmail.com>
2025-10-31 02:57:25 +0300

    [TASK-3] Adding models

---
+++ orlova.alena/task-3/internal/models/models.go
@@ -0,0 +1,25 @@
+package models
+
+import "encoding/xml"
+
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Date    string   `xml:"Date,attr"`
+	Valutes []Valute `xml:"Valute"`
+}
+
+type Valute struct {
+	XMLName  xml.Name `xml:"Valute"`
+	ID       string   `xml:"ID,attr"`
+	NumCode  string   `xml:"NumCode"`
+	CharCode string   `xml:"CharCode"`
+	Nominal  int      `xml:"Nominal"`
+	Name     string   `xml:"Name"`
+	Value    string   `xml:"Value"`
+}
+
+type Currency struct {
+	NumCode  string  `json:"num_code"`
+	CharCode string  `json:"char_code"`
+	Value    float64 `json:"value"`
+}
