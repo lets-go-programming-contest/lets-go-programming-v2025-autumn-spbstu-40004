aa4ce4cb4a6016ccbf875a7d12d07ac49207eea3
15446-rus75 <vladabramov06@yandex.ru>
2025-10-30 17:45:07 +0300

    [TASK-3] update main.go

--- vladislav.abramov/task-3/cmd/service/main.go
+++ vladislav.abramov/task-3/cmd/service/main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"flag"
+
+	"currency-processor/internal/parser"
+	"currency-processor/internal/processor"
+	"currency-processor/internal/ioutils"
+)
+
 func main() {
+	configPath := flag.String("config", "", "Path to YAML configuration file")
+	flag.Parse()
+
+	if *configPath == "" {
+		panic("configuration file path must be provided using --config flag")
+	}
+
+	cfg, err := config.LoadConfiguration(*configPath)
+	if err != nil {
+		panic("failed to load configuration: " + err.Error())
+	}
+
+	currencyData, err := parser.ParseCurrencyData(cfg.InputFile)
+	if err != nil {
+		panic("failed to parse currency data: " + err.Error())
+	}
+
+	sortedCurrencies, err := processor.SortCurrenciesByValue(currencyData.Valutes)
+	if err != nil {
+		panic("failed to process currency data: " + err.Error())
+	}
+
+	err = writer.WriteJSONOutput(sortedCurrencies, cfg.OutputFile)
+	if err != nil {
+		panic("failed to write output file: " + err.Error())
+	}
 }
