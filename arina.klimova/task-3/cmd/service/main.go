aacdcf9e1c371430908094ced8ffce9d4f4d9fc1
arinaklimova <arina.klim.06@mail.ru>
2025-10-30 19:23:48 +0300

    [TASK-3] Initialize project structure and models (save files)

--- arina.klimova/task-3/cmd/service/main.go
+++ arina.klimova/task-3/cmd/service/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
 func main() {
-	fmt.Println("task-3")
+	configPath := flag.String("config", "", "Path to configuration file")
+	flag.Parse()
+
+	if *configPath == "" {
+		panic("Config file path is required")
+	}
+
+	fmt.Printf("Config path: %s\n", *configPath)
 }
