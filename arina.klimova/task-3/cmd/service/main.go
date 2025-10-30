f36d090e2a999c97c43221078844501e9d6ea917
arinaklimova <arina.klim.06@mail.ru>
2025-10-30 20:20:29 +0300

    [TASK-3] Update main

--- arina.klimova/task-3/cmd/service/main.go
+++ arina.klimova/task-3/cmd/service/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/arinaklimova/task-3/internal/config"
+	"github.com/arinaklimova/task-3/internal/parser"
+	"github.com/arinaklimova/task-3/internal/sorter"
+	"github.com/arinaklimova/task-3/internal/writer"
 )
 
 func main() {
 	cfg := config.LoadConfig()
+	valCurs, err := parser.ParseXML(cfg.InputFile)
+	if err != nil {
+		panic("failed to parse XML file: " + err.Error())
+	}
 
-	fmt.Printf("Configuration loaded successfully:")
-	fmt.Printf("Input file: %s\n", cfg.InputFile)
-	fmt.Printf("Output file: %s\n", cfg.OutputFile)
+	sorter.SortCurrenciesByValueDesc(valCurs.Currencies)
+
+	if err := writer.WriteJSON(valCurs.Currencies, cfg.OutputFile); err != nil {
+		panic("failed to write JSON file: " + err.Error())
+	}
 }
