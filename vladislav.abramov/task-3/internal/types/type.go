2c6b2b6eeac62b481769ec9feb50068d2f6dc81d
15446-rus75 <vladabramov06@yandex.ru>
2025-10-30 16:50:00 +0300

    [TASK-3] add type.go

---
+++ vladislav.abramov/task-3/internal/types/type.go
@@ -0,0 +1,25 @@
+package types
+
+import "encoding/xml"
+
+type CurrencyData struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Date    string   `xml:"Date,attr"`
+	Valutes []Valute `xml:"Valute"`
+}
+
+type Valute struct {
+	XMLName  xml.Name `xml:"Valute" json:"-"`
+	ID       string   `xml:"ID,attr" json:"-"`
+	NumCode  int      `xml:"NumCode" json:"num_code"`
+	CharCode string   `xml:"CharCode" json:"char_code"`
+	Nominal  int      `xml:"Nominal" json:"-"`
+	Name     string   `xml:"Name" json:"-"`
+	Value    string   `xml:"Value" json:"-"`
+}
+
+type CurrencyOutput struct {
+	NumCode  int     `json:"num_code"`
+	CharCode string  `json:"char_code"`
+	Value    float64 `json:"value"`
+}
