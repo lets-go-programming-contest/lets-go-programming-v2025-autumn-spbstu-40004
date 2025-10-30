31bb03f64b645aaf7251a154c4f289464a63025e
arinaklimova <arina.klim.06@mail.ru>
2025-10-30 19:24:52 +0300

    [TASK-3] Add flag parsing for config path

--- arina.klimova/task-3/internal/config/config.go
+++ arina.klimova/task-3/internal/config/config.go
@@ -1,6 +1,20 @@
 package config
 
+import "flag"
+
 type Config struct {
 	InputFile  string `yaml:"input-file"`
 	OutputFile string `yaml:"output-file"`
+}
+
+func LoadConfigPath() string {
+	var configPath string
+	flag.StringVar(&configPath, "config", "", "path to config file")
+	flag.Parse()
+
+	if configPath == "" {
+		panic("config flag is required")
+	}
+
+	return configPath
 }
