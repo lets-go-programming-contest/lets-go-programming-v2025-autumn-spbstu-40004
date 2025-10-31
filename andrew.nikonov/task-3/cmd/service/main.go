09f2a73c0338b500ea7a04ea88b890614be1da73
ysffmn <andrewnikonov387@gmail.com>
2025-10-31 23:44:28 +0300

    [TASK-3] main refactoring

--- andrew.nikonov/task-3/cmd/service/main.go
+++ andrew.nikonov/task-3/cmd/service/main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 
 	"github.com/ysffmn/task-3/internal/config"
+	"github.com/ysffmn/task-3/internal/parser"
+	"github.com/ysffmn/task-3/internal/processor"
+	"github.com/ysffmn/task-3/internal/writer"
 )
 
 func main() {
@@ -15,5 +18,9 @@ func main() {
 	}
 
 	cfg := config.Load(*configPath)
-	_ = cfg
+
+	valCurs := parser.ParseXMLFile(cfg.InputFile)
+	currencies := parser.ConvertToCurrencies(valCurs)
+	sortedCurrencies := processor.SortCurrenciesDesc(currencies)
+	writer.WriteJSONToFile(sortedCurrencies, cfg.OutputFile)
 }
