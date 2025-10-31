51186554fac7b5f27f475508bb455bf18c465d5a
ysffmn <andrewnikonov387@gmail.com>
2025-10-31 23:23:15 +0300

    [TASK-3] currency structures added

---
+++ andrew.nikonov/task-3/internal/currency/currency.go
@@ -0,0 +1,25 @@
+package currency
+
+import "encoding/xml"
+
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Date    string   `xml:"Date,attr"`
+	Name    string   `xml:"name,attr"`
+	Valutes []Valute `xml:"Valute"`
+}
+
+type Valute struct {
+	ID       string `xml:"ID,attr"`
+	NumCode  string `xml:"NumCode"`
+	CharCode string `xml:"CharCode"`
+	Nominal  int    `xml:"Nominal"`
+	Name     string `xml:"Name"`
+	Value    string `xml:"Value"`
+}
+
+type Currency struct {
+	NumCode  int     `json:"num_code"`
+	CharCode string  `json:"char_code"`
+	Value    float64 `json:"value"`
+}
