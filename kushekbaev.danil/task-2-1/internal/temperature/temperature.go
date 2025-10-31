7e33db7c42ed3f579d90107829a3bb5c98b7877e
danil@debian1337 <un228der@gmail.com>
2025-10-31 11:30:56 +0300

    [TASK-2-1] possible lint fix 2

--- kushekbaev.danil/task-2-1/internal/temperature/temperature.go
+++ kushekbaev.danil/task-2-1/internal/temperature/temperature.go
@@ -8,14 +8,16 @@ var (
 	ErrInvalidOp = errors.New("invalid operator")
 )
 
+const {
+	invalidValue int = -1;
+}
+
 type Temperature struct {
 	UpperBound int
 	LowerBound int
 }
 
 func (temp *Temperature) SetTemperature(operator string, desirableTemp int) (int, error) {
-	const invalidValue int = -1;
-
 	switch operator {
 	case "<=":
 		temp.UpperBound = ternaryInt(temp.UpperBound < desirableTemp, temp.UpperBound, desirableTemp)
