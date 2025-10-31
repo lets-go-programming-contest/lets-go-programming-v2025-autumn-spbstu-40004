e1d5cdcfb328ec38b8771d8b77fd91496819e51c
danil@debian1337 <un228der@gmail.com>
2025-10-31 22:36:39 +0300

    [TASK-3] Initial commit

---
+++ kushekbaev.danil/task-3/internal/currencyparser/currencyparser.go
@@ -0,0 +1,48 @@
+package currencyparser
+
+import (
+	"encoding/xml"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type DotFloat float64
+
+type Currency struct {
+	NumCode  int      `json:"num_code"  xml:"NumCode"`
+	CharCode string   `json:"char_code" xml:"CharCode"`
+	Value    DotFloat `json:"value"     xml:"Value"`
+}
+
+type Currencies struct {
+	AllCurrencies []Currency `xml:"Currency"`
+}
+
+func Sort(currencies []Currency) []Currency {
+	sort.Slice(currencies, func(i, j int) bool {
+		return currencies[i].Value > currencies[j].Value
+	})
+
+	return currencies
+}
+
+func (dotFloat *DotFloat) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
+	var str string
+
+	err := decoder.DecodeElement(&str, &start)
+	if err != nil {
+		return err
+	}
+
+	str = strings.ReplaceAll(str, ",", ".")
+
+	num, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return err
+	}
+
+	*dotFloat = DotFloat(num)
+
+	return nil
+}
