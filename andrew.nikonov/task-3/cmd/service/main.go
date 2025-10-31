9de97b3acb662ce3b29df0fce883945ac20ead3b
ysffmn <andrewnikonov387@gmail.com>
2025-10-31 23:20:42 +0300

    [TASK-3] simple main for config reading

--- andrew.nikonov/task-3/cmd/service/main.go
+++ andrew.nikonov/task-3/cmd/service/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"flag"
+
+	"github.com/ysffmn/task-3/internal/config"
+)
+
+func main() {
+	configPath := flag.String("config", "", "Path to config file")
+	flag.Parse()
+
+	if *configPath == "" {
+		panic("Config flag is required")
+	}
+
+	cfg := config.Load(*configPath)
+	_ = cfg
+}
