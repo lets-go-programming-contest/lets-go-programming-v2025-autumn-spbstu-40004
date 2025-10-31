cf1a6f26368539cba710a425f14ce764d22343c0
cherkasoov <cherkasov5509@gmail.com>
2025-10-31 23:44:46 +0300

    [TASK-3] Add config main

--- nikita.cherkasov/task-3/cmd/service/main.go
+++ nikita.cherkasov/task-3/cmd/service/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"flag"
+
+	"github.com/cherkasoov/task-3/internal/config"
+)
+
 func main() {
-	
+	configPath := flag.String("config", "", "path to YAML config file")
+	flag.Parse()
+
+	if *configPath == "" {
+		panic("--config flag is required")
+	}
+
+	_, err := config.LoadConfig(*configPath)
+	if err != nil {
+		panic(err)
+	}
 }
