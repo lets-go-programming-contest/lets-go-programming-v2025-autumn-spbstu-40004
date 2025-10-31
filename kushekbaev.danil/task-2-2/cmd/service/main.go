8ec5c0ecf29ad62dcdf84ca530bd9f25eb62feb5
danil@debian1337 <un228der@gmail.com>
2025-10-31 22:24:51 +0300

    [TASK-2-2] comment fixes

--- kushekbaev.danil/task-2-2/cmd/service/main.go
+++ kushekbaev.danil/task-2-2/cmd/service/main.go
@@ -3,43 +3,9 @@ package main
 import (
 	"container/heap"
 	"fmt"
+
+	"github.com/Z-1337/task-2-2/internal/maxheap"
 )
-
-type MaxHeap []int
-
-func (maxHeap *MaxHeap) Len() int {
-	return len(*maxHeap)
-}
-
-func (maxHeap *MaxHeap) Less(indexFirst, indexSecond int) bool {
-	return (*maxHeap)[indexFirst] >= (*maxHeap)[indexSecond]
-}
-
-func (maxHeap *MaxHeap) Swap(indexFirst, indexSecond int) {
-	(*maxHeap)[indexFirst], (*maxHeap)[indexSecond] = (*maxHeap)[indexSecond], (*maxHeap)[indexFirst]
-}
-
-func (maxHeap *MaxHeap) Push(value any) {
-	val, ok := value.(int)
-	if !ok {
-		panic("Value must be int in MaxHeap")
-	}
-
-	*maxHeap = append(*maxHeap, val)
-}
-
-func (maxHeap *MaxHeap) Pop() any {
-	oldLen := len(*maxHeap)
-	if oldLen == 0 {
-		return nil
-	}
-
-	oldHeap := *maxHeap
-	poppedValue := oldHeap[oldLen-1]
-	*maxHeap = oldHeap[0 : oldLen-1]
-
-	return poppedValue
-}
 
 func main() {
 	var (
@@ -55,7 +21,7 @@ func main() {
 		return
 	}
 
-	preferences := &MaxHeap{}
+	preferences := &maxheap.MaxHeap{}
 	heap.Init(preferences)
 
 	for range amount {
