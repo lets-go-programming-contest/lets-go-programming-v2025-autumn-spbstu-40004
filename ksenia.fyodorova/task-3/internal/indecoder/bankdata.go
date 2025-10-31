9b717631945e6fee9fe25bad3e4d719aab68dc2d
Ksenia <f.kcusha082@mail.ru>
2025-10-31 23:10:37 +0300

    [TASK-3] Fix lint

--- ksenia.fyodorova/task-3/internal/indecoder/bankdata.go
+++ ksenia.fyodorova/task-3/internal/indecoder/bankdata.go
@@ -4,17 +4,14 @@ type CurrencyCollection struct {
 	Items []CurrencyItem `xml:"Valute"`
 }
 
-// Len возвращает количество элементов
 func (cc CurrencyCollection) Len() int {
 	return len(cc.Items)
 }
 
-// Swap меняет местами элементы i и j
 func (cc CurrencyCollection) Swap(i, j int) {
 	cc.Items[i], cc.Items[j] = cc.Items[j], cc.Items[i]
 }
 
-// Less определяет порядок сортировки (по убыванию значения)
 func (cc CurrencyCollection) Less(i, j int) bool {
 	return cc.Items[i].ConvertedAmount > cc.Items[j].ConvertedAmount
 }
--- ksenia.fyodorova/task-3/internal/outcoder/outcoder.go
+++ ksenia.fyodorova/task-3/internal/outcoder/outcoder.go
@@ -2,27 +2,28 @@ package outcoder
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 
-	"github.com/lolnyok/task-3/internal/indecoder"
+	"github.com/Ksenia-rgb/task-3/internal/indecoder"
 )
 
 func SaveCurrencyData(outputPath string, data indecoder.CurrencyCollection) error {
 	dir := filepath.Dir(outputPath)
-	err := os.MkdirAll(dir, 0755)
+	err := os.MkdirAll(dir, 0o755)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create directory: %w", err)
 	}
 
 	jsonData, err := json.MarshalIndent(data.Items, "", "  ")
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to marshal JSON: %w", err)
 	}
 
-	err = os.WriteFile(outputPath, jsonData, 0644)
+	err = os.WriteFile(outputPath, jsonData, 0o600)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to write file: %w", err)
 	}
 
 	return nil
