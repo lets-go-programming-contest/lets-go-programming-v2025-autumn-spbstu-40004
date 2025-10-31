ebb325166efa4c3444ac13e882ef743a1997516d
cherkasoov <cherkasov5509@gmail.com>
2025-10-31 23:49:56 +0300

    [TASK-3] Add main

--- nikita.cherkasov/task-3/internal/model/currency.go
+++ nikita.cherkasov/task-3/internal/model/currency.go
@@ -1,21 +1,39 @@
-package model
+package models
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-type XMLValCurs struct {
-	XMLName xml.Name    `xml:"ValCurs"`
-	Valutes []XMLValute `xml:"Valute"`
+type CurrencyValue float64
+
+func (v *CurrencyValue) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var str string
+	if err := d.DecodeElement(&str, &start); err != nil {
+		return fmt.Errorf("failed to unmarshal currency value: %w", err)
+	}
+
+	str = strings.ReplaceAll(str, ",", ".")
+
+	value, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return fmt.Errorf("failed to parse currency value: %w", err)
+	}
+
+	*v = CurrencyValue(value)
+
+	return nil
 }
 
-type XMLValute struct {
-	XMLName  xml.Name `xml:"Valute"`
-	NumCode  string   `xml:"NumCode"`
-	CharCode string   `xml:"CharCode"`
-	Value    string   `xml:"Value"`
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Valutes []Valute `xml:"Valute"`
 }
 
-type JSONValute struct {
-	NumCode  int     `json:"num_code"`
-	CharCode string  `json:"char_code"`
-	Value    float64 `json:"value"`
+type Valute struct {
+	NumCode  int           `json:"num_code"  xml:"NumCode"`
+	CharCode string        `json:"char_code" xml:"CharCode"`
+	Value    CurrencyValue `json:"value"     xml:"Value"`
 }
