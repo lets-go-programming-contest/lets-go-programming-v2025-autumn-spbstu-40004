7055989a092945b064c7d5324e84aa10abd1ee06
15446-rus75 <vladabramov06@yandex.ru>
2025-11-25 19:47:48 +0300

    [TASK-5] some lint fixes

--- vladislav.abramov/task-5/pkg/conveyer/conveyer.go
+++ vladislav.abramov/task-5/pkg/conveyer/conveyer.go
@@ -3,20 +3,23 @@ package conveyer
 import (
 	"context"
 	"errors"
+	"fmt"
+
 	"golang.org/x/sync/errgroup"
 )
 
 var (
 	ErrChanNotFound = errors.New("chan not found")
-	ErrUndefined    = "undefined"
 )
 
+const errUndefined = "undefined"
+
 type conveyer struct {
-	channels    map[string]chan string
-	size        int
-	decorators  []decoratorConfig
+	channels     map[string]chan string
+	size         int
+	decorators   []decoratorConfig
 	multiplexers []multiplexerConfig
-	separators  []separatorConfig
+	separators   []separatorConfig
 }
 
 type decoratorConfig struct {
@@ -39,30 +42,35 @@ type separatorConfig struct {
 
 func New(size int) *conveyer {
 	return &conveyer{
-		channels: make(map[string]chan string),
-		size:     size,
+		channels:     make(map[string]chan string),
+		size:         size,
+		decorators:   make([]decoratorConfig, 0),
+		multiplexers: make([]multiplexerConfig, 0),
+		separators:   make([]separatorConfig, 0),
 	}
 }
 
 func (c *conveyer) getOrCreateChannel(name string) chan string {
-	if ch, exists := c.channels[name]; exists {
-		return ch
+	if channel, exists := c.channels[name]; exists {
+		return channel
 	}
 
-	ch := make(chan string, c.size)
-	c.channels[name] = ch
-	return ch
+	channel := make(chan string, c.size)
+	c.channels[name] = channel
+
+	return channel
 }
 
 func (c *conveyer) getChannel(name string) (chan string, error) {
-	if ch, exists := c.channels[name]; exists {
-		return ch, nil
+	if channel, exists := c.channels[name]; exists {
+		return channel, nil
 	}
+
 	return nil, ErrChanNotFound
 }
 
 func (c *conveyer) RegisterDecorator(
-	fn func(ctx context.Context, input chan string, output chan string) error,
+	decoratorFunc func(ctx context.Context, input chan string, output chan string) error,
 	input string,
 	output string,
 ) {
@@ -70,41 +78,43 @@ func (c *conveyer) RegisterDecorator(
 	c.getOrCreateChannel(output)
 
 	c.decorators = append(c.decorators, decoratorConfig{
-		fn:     fn,
+		fn:     decoratorFunc,
 		input:  input,
 		output: output,
 	})
 }
 
 func (c *conveyer) RegisterMultiplexer(
-	fn func(ctx context.Context, inputs []chan string, output chan string) error,
+	multiplexerFunc func(ctx context.Context, inputs []chan string, output chan string) error,
 	inputs []string,
 	output string,
 ) {
-	for _, input := range inputs {
-		c.getOrCreateChannel(input)
+	for _, inputName := range inputs {
+		c.getOrCreateChannel(inputName)
 	}
+
 	c.getOrCreateChannel(output)
 
 	c.multiplexers = append(c.multiplexers, multiplexerConfig{
-		fn:     fn,
+		fn:     multiplexerFunc,
 		inputs: inputs,
 		output: output,
 	})
 }
 
 func (c *conveyer) RegisterSeparator(
-	fn func(ctx context.Context, input chan string, outputs []chan string) error,
+	separatorFunc func(ctx context.Context, input chan string, outputs []chan string) error,
 	input string,
 	outputs []string,
 ) {
 	c.getOrCreateChannel(input)
-	for _, output := range outputs {
-		c.getOrCreateChannel(output)
+
+	for _, outputName := range outputs {
+		c.getOrCreateChannel(outputName)
 	}
 
 	c.separators = append(c.separators, separatorConfig{
-		fn:      fn,
+		fn:      separatorFunc,
 		input:   input,
 		outputs: outputs,
 	})
@@ -113,69 +123,81 @@ func (c *conveyer) RegisterSeparator(
 func (c *conveyer) Run(ctx context.Context) error {
 	defer c.closeAllChannels()
 
-	g, ctx := errgroup.WithContext(ctx)
+	errorGroup, groupCtx := errgroup.WithContext(ctx)
 
 	for _, decorator := range c.decorators {
-		inputChan, _ := c.getChannel(decorator.input)
-		outputChan, _ := c.getChannel(decorator.output)
+		inputChannel, _ := c.getChannel(decorator.input)
+		outputChannel, _ := c.getChannel(decorator.output)
 
-		g.Go(func() error {
-			return decorator.fn(ctx, inputChan, outputChan)
+		currentDecorator := decorator
+		errorGroup.Go(func() error {
+			return currentDecorator.fn(groupCtx, inputChannel, outputChannel)
 		})
 	}
 
 	for _, multiplexer := range c.multiplexers {
-		inputChans := make([]chan string, len(multiplexer.inputs))
-		for i, input := range multiplexer.inputs {
-			inputChans[i], _ = c.getChannel(input)
-		}
-		outputChan, _ := c.getChannel(multiplexer.output)
+		inputChannels := make([]chan string, len(multiplexer.inputs))
 
-		g.Go(func() error {
-			return multiplexer.fn(ctx, inputChans, outputChan)
+		for index, inputName := range multiplexer.inputs {
+			inputChannels[index], _ = c.getChannel(inputName)
+		}
+
+		outputChannel, _ := c.getChannel(multiplexer.output)
+
+		currentMultiplexer := multiplexer
+		errorGroup.Go(func() error {
+			return currentMultiplexer.fn(groupCtx, inputChannels, outputChannel)
 		})
 	}
 
 	for _, separator := range c.separators {
-		inputChan, _ := c.getChannel(separator.input)
-		outputChans := make([]chan string, len(separator.outputs))
-		for i, output := range separator.outputs {
-			outputChans[i], _ = c.getChannel(output)
+		inputChannel, _ := c.getChannel(separator.input)
+		outputChannels := make([]chan string, len(separator.outputs))
+
+		for index, outputName := range separator.outputs {
+			outputChannels[index], _ = c.getChannel(outputName)
 		}
 
-		g.Go(func() error {
-			return separator.fn(ctx, inputChan, outputChans)
+		currentSeparator := separator
+		errorGroup.Go(func() error {
+			return currentSeparator.fn(groupCtx, inputChannel, outputChannels)
 		})
 	}
 
-	return g.Wait()
+	if err := errorGroup.Wait(); err != nil {
+		return fmt.Errorf("conveyer finished with error: %w", err)
+	}
+
+	return nil
 }
 
 func (c *conveyer) closeAllChannels() {
-	for _, ch := range c.channels {
-		close(ch)
+	for _, channel := range c.channels {
+		close(channel)
 	}
 }
 
 func (c *conveyer) Send(input string, data string) error {
-	ch, err := c.getChannel(input)
+	channel, err := c.getChannel(input)
 	if err != nil {
 		return err
 	}
 
-	ch <- data
+	channel <- data
+
 	return nil
 }
 
 func (c *conveyer) Recv(output string) (string, error) {
-	ch, err := c.getChannel(output)
+	channel, err := c.getChannel(output)
 	if err != nil {
 		return "", err
 	}
 
-	data, ok := <-ch
+	data, ok := <-channel
 	if !ok {
-		return ErrUndefined, nil
+		return errUndefined, nil
 	}
+
 	return data, nil
 }
