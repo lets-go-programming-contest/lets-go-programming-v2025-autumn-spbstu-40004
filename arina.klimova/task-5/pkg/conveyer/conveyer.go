6e924cc380dce2d0ee1a854f36481124d46b3b3e
arinaklimova <arina.klim.06@mail.ru>
2025-11-30 22:30:37 +0300

    [TASK-5] method Run, cleanup

--- arina.klimova/task-5/pkg/conveyer/conveyer.go
+++ arina.klimova/task-5/pkg/conveyer/conveyer.go
@@ -2,14 +2,17 @@ package conveyer
 
 import (
 	"context"
+	"fmt"
 	"sync"
 )
 
 type conveyer struct {
-	mu       sync.RWMutex
-	channels map[string]chan string
-	handlers []handlerFunc
-	size     int
+	mu          sync.RWMutex
+	channels    map[string]chan string
+	handlers    []handlerFunc
+	size        int
+	isRunning   bool
+	cancelFuncs []context.CancelFunc
 }
 
 type handlerFunc struct {
@@ -20,9 +23,10 @@ type handlerFunc struct {
 
 func New(size int) *conveyer {
 	return &conveyer{
-		channels: make(map[string]chan string),
-		handlers: make([]handlerFunc, 0),
-		size:     size,
+		channels:    make(map[string]chan string),
+		handlers:    make([]handlerFunc, 0),
+		size:        size,
+		cancelFuncs: make([]context.CancelFunc, 0),
 	}
 }
 
@@ -95,4 +99,90 @@ func (c *conveyer) RegisterSeparator(
 		inputs:  []string{input},
 		outputs: outputs,
 	})
+}
+
+func (c *conveyer) Run(ctx context.Context) error {
+	c.mu.Lock()
+	if c.isRunning {
+		c.mu.Unlock()
+		return fmt.Errorf("conveyer is already running")
+	}
+	c.isRunning = true
+	c.mu.Unlock()
+
+	defer c.cleanup()
+
+	handlerCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var wg sync.WaitGroup
+	errorCh := make(chan error, len(c.handlers))
+
+	for _, handler := range c.handlers {
+		wg.Add(1)
+		go func(h handlerFunc) {
+			defer wg.Done()
+
+			var err error
+			switch fn := h.fn.(type) {
+			case func(ctx context.Context, input chan string, output chan string) error:
+				inputChan := c.getOrCreateChannel(h.inputs[0])
+				outputChan := c.getOrCreateChannel(h.outputs[0])
+				err = fn(handlerCtx, inputChan, outputChan)
+			case func(ctx context.Context, inputs []chan string, output chan string) error:
+				inputChans := make([]chan string, len(h.inputs))
+				for i, input := range h.inputs {
+					inputChans[i] = c.getOrCreateChannel(input)
+				}
+				outputChan := c.getOrCreateChannel(h.outputs[0])
+				err = fn(handlerCtx, inputChans, outputChan)
+			case func(ctx context.Context, input chan string, outputs []chan string) error:
+				inputChan := c.getOrCreateChannel(h.inputs[0])
+				outputChans := make([]chan string, len(h.outputs))
+				for i, output := range h.outputs {
+					outputChans[i] = c.getOrCreateChannel(output)
+				}
+				err = fn(handlerCtx, inputChan, outputChans)
+			default:
+				err = fmt.Errorf("unknown handler type")
+			}
+
+			if err != nil {
+				select {
+				case errorCh <- err:
+				default:
+				}
+				cancel()
+			}
+		}(handler)
+	}
+
+	go func() {
+		wg.Wait()
+		close(errorCh)
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err, ok := <-errorCh:
+		if ok && err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *conveyer) cleanup() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for name, ch := range c.channels {
+		close(ch)
+		delete(c.channels, name)
+	}
+	c.handlers = make([]handlerFunc, 0)
+	c.isRunning = false
+	c.cancelFuncs = make([]context.CancelFunc, 0)
 }
