bee487bbd313aed349ffffb343578788982704ae
arinaklimova <arina.klim.06@mail.ru>
2025-11-30 22:48:03 +0300

    [TASK-5] handler functions

--- arina.klimova/task-5/pkg/handlers/handlers.go
+++ arina.klimova/task-5/pkg/handlers/handlers.go
@@ -2,16 +2,117 @@ package handlers
 
 import (
 	"context"
+	"errors"
+	"strings"
+	"sync"
 )
 
 func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
-	return nil
+	const prefix = "decorated: "
+	const errorSubstring = "no decorator"
+	const errorMessage = "can't be decorated"
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, errorSubstring) {
+				return errors.New(errorMessage)
+			}
+
+			var result string
+			if strings.HasPrefix(data, prefix) {
+				result = data
+			} else {
+				result = prefix + data
+			}
+
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case output <- result:
+			}
+		}
+	}
 }
 
 func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
-	return nil
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			outputIndex := len(data) % len(outputs)
+
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case outputs[outputIndex] <- data:
+			}
+		}
+	}
 }
 
 func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
-	return nil
+	const filterSubstring = "no multiplexer"
+
+	mergedInput := make(chan string)
+
+	var wg sync.WaitGroup
+	for _, input := range inputs {
+		wg.Add(1)
+		go func(in chan string) {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case data, ok := <-in:
+					if !ok {
+						return
+					}
+					select {
+					case <-ctx.Done():
+						return
+					case mergedInput <- data:
+					}
+				}
+			}
+		}(input)
+	}
+
+	go func() {
+		wg.Wait()
+		close(mergedInput)
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case data, ok := <-mergedInput:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, filterSubstring) {
+				continue
+			}
+
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case output <- data:
+			}
+		}
+	}
 }
