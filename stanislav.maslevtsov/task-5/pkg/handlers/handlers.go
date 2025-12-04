9ef2e60bbdbc07a7283900dc1f5023e082244514
jambii1 <s.maslevtsov@gmail.com>
2025-12-04 16:09:21 +0300

    [TASK-5] Add decorator, multiplexer and separator handlers

--- stanislav.maslevtsov/task-5/pkg/handlers/handlers.go
+++ stanislav.maslevtsov/task-5/pkg/handlers/handlers.go
@@ -1,18 +1,80 @@
 package handlers
 
-import "context"
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+)
+
+var (
+	ErrNoDecorator = errors.New("can't be decorated")
+)
 
 func PrefixDecoratorFunc(
 	ctx context.Context,
 	input, output chan string,
 ) error {
-	return nil
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case str, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(str, "no decorator") {
+				return ErrNoDecorator
+			}
+
+			if !strings.HasPrefix(str, "decorated:") {
+				str = "decorated:" + str
+			}
+
+			select {
+			case <-ctx.Done():
+				return nil
+			case output <- str:
+			}
+		}
+	}
 }
 
 func MultiplexerFunc(ctx context.Context,
 	inputs []chan string,
 	output chan string,
 ) error {
+	var waitGr sync.WaitGroup
+	waitGr.Add(len(inputs))
+
+	for ichanIdx := range inputs {
+		go func() {
+			defer waitGr.Done()
+
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case str, ok := <-inputs[ichanIdx]:
+					if !ok {
+						return
+					}
+
+					if !strings.Contains(str, "no multiplexer") {
+						select {
+						case <-ctx.Done():
+							return
+						case output <- str:
+						}
+					}
+				}
+			}
+		}()
+	}
+
+	waitGr.Wait()
+
 	return nil
 }
 
@@ -21,5 +83,23 @@ func SeparatorFunc(
 	input chan string,
 	outputs []chan string,
 ) error {
-	return nil
+	var ochanIdx int
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case str, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			select {
+			case <-ctx.Done():
+				return nil
+			case outputs[ochanIdx] <- str:
+			}
+
+			ochanIdx = (ochanIdx + 1) % len(outputs)
+		}
+	}
 }
