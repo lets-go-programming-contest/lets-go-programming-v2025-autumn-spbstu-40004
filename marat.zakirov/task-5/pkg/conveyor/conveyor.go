17a3a6adad2675d39de4626203a2f924ffd355c4
ZakirovMS <mz.08.08.06@gmail.com>
2025-12-05 23:45:54 +0300

    [TASK-5] Implementation of register function

--- marat.zakirov/task-5/pkg/conveyor/conveyor.go
+++ marat.zakirov/task-5/pkg/conveyor/conveyor.go
@@ -4,21 +4,21 @@ import (
 	"context"
 )
 
-type Сonveyor struct {
+type Conveyor struct {
 	channels map[string]chan string
 	handlers []func(ctx context.Context) error
 	size     int
 }
 
-func New(size int) Сonveyor {
-	return Сonveyor{
+func New(size int) Conveyor {
+	return Conveyor{
 		channels: make(map[string]chan string),
 		handlers: make([]func(ctx context.Context) error, 0),
 		size:     size,
 	}
 }
 
-func (c *Сonveyor) createChan(chName string) chan string {
+func (c *Conveyor) createChannel(chName string) chan string {
 	if channel, exists := c.channels[chName]; exists {
 		return channel
 	}
@@ -29,31 +29,50 @@ func (c *Сonveyor) createChan(chName string) chan string {
 	return newChannel
 }
 
-func (c *Сonveyor) RegisterDecorator(
+func (c *Conveyor) RegisterDecorator(
 	fn func(cntx context.Context, in chan string, out chan string) error,
 	in string,
-	out string) {
+	out string,
+) {
+	inChannel := c.createChannel(in)
+	outChannel := c.createChannel(out)
 
+	c.handlers = append(c.handlers, func(cntx context.Context) error {
+		return fn(cntx, inChannel, outChannel)
+	})
 }
 
-func (c *Сonveyor) RegisterMultiplexer(
-	fn func(
-		ctx context.Context,
-		inputs []chan string,
-		output chan string,
-	) error,
-	inputs []string,
-	output string,
-)
-func (c *Сonveyor) RegisterSeparator(
-	fn func(
-		ctx context.Context,
-		input chan string,
-		outputs []chan string,
-	) error,
-	input string,
-	outputs []string,
-)
+func (c *Conveyor) RegisterMultiplexer(
+	fn func(ctx context.Context, ins []chan string, out chan string) error,
+	ins []string,
+	out string,
+) {
+	inChannels := make([]chan string, len(ins))
+	for i, name := range ins {
+		inChannels[i] = c.createChannel(name)
+	}
+	outChannel := c.createChannel(out)
+
+	c.handlers = append(c.handlers, func(cntx context.Context) error {
+		return fn(cntx, inChannels, outChannel)
+	})
+}
+
+func (c *Conveyor) RegisterSeparator(
+	fn func(ctx context.Context, in chan string, outs []chan string) error,
+	in string,
+	outs []string,
+) {
+	inChannel := c.createChannel(in)
+	outChannels := make([]chan string, len(outs))
+	for i, name := range outs {
+		outChannels[i] = c.createChannel(name)
+	}
+
+	c.handlers = append(c.handlers, func(cntx context.Context) error {
+		return fn(cntx, inChannel, outChannels)
+	})
+}
 
 func Run(ctx context.Context) error {
 	return nil
