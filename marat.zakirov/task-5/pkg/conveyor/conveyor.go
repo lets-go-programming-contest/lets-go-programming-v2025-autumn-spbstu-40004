63474f4a4dcdc3f5672c088e3bb4cce1a36336a8
ZakirovMS <mz.08.08.06@gmail.com>
2025-12-05 22:45:42 +0300

    [TASK-5] Implementation of new function

--- marat.zakirov/task-5/pkg/conveyor/conveyor.go
+++ marat.zakirov/task-5/pkg/conveyor/conveyor.go
@@ -1,1 +1,31 @@
 package conveyor
+
+import (
+	"context"
+)
+
+type Сonveyor struct {
+	channels map[string]chan string
+	handlers []func(ctx context.Context) error
+	size     int
+}
+
+func New(size int) Сonveyor {
+	return Сonveyor{
+		channels: make(map[string]chan string),
+		handlers: make([]func(ctx context.Context) error, 0),
+		size:     size,
+	}
+}
+
+func Run(ctx context.Context) error {
+	return nil
+}
+
+func Send(input string, data string) error {
+	return nil
+}
+
+func Recv(output string) (string, error) {
+	return "nil", nil
+}
