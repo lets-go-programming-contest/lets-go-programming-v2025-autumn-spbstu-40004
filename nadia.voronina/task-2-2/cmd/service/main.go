0d164eebce6cb8318e28dc9c8d87325172d91f84
Nadia <nadia.v.voronina@gmail.com>
2025-12-05 17:31:21 +0300

    [TASK-2-2] Review fixes

--- nadia.voronina/task-2-2/cmd/service/main.go
+++ nadia.voronina/task-2-2/cmd/service/main.go
@@ -2,46 +2,29 @@ package main
 
 import (
 	"container/heap"
+	"errors"
 	"fmt"
+
+	"spbstu.ru/nadia.voronina/task-2-2/pkg/intheap"
 )
 
-type IntHeap []int
-
-func (h *IntHeap) Len() int           { return len(*h) }
-func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
-func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
-
-func (h *IntHeap) Push(x interface{}) {
-	value, err := x.(int)
-	if !err {
-		fmt.Println("Invalid type")
-
-		return
-	} else {
-		*h = append(*h, value)
-	}
-}
-
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-
-	return x
-}
+var (
+	ErrInvalidNumberOfDishes = errors.New("invalid number of dishes")
+	ErrInvalidDish           = errors.New("invalid dish")
+	ErrInvalidPreferredDish  = errors.New("invalid preferred dish")
+)
 
 func main() {
 	var numberOfDishes int
 
 	_, errN := fmt.Scanln(&numberOfDishes)
 	if errN != nil {
-		fmt.Println("Invalid number of dishes")
+		fmt.Println(ErrInvalidNumberOfDishes)
 
 		return
 	}
 
-	var dishes IntHeap
+	var dishes intheap.IntHeap
 
 	heap.Init(&dishes)
 
@@ -51,7 +34,7 @@ func main() {
 		_, errK := fmt.Scan(&dish)
 
 		if errK != nil {
-			fmt.Println("Invalid dish")
+			fmt.Println(ErrInvalidDish)
 
 			return
 		}
@@ -64,7 +47,7 @@ func main() {
 	_, errK := fmt.Scan(&preferredDish)
 
 	if errK != nil {
-		fmt.Println("Invalid preferred dish")
+		fmt.Println(ErrInvalidPreferredDish)
 
 		return
 	}
---
+++ nadia.voronina/task-2-2/pkg/intheap/intheap.go
@@ -0,0 +1,29 @@
+package intheap
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *IntHeap) Push(x any) {
+	value, err := x.(int)
+	if !err {
+		panic("Invalid type")
+
+	} else {
+		*h = append(*h, value)
+	}
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	if n == 0 {
+		panic("Pop from empty heap")
+	}
+	x := old[n-1]
+	*h = old[0 : n-1]
+
+	return x
+}
