e8ddc99320fa26d98362f8e56fff524049f07eda
cherkasoov <cherkasov5509@gmail.com>
2025-12-07 00:00:32 +0300

    [TASK-5] add func separator

--- nikita.cherkasov/task-5/cmd/service/pkg/handlers/handlers.go
+++ nikita.cherkasov/task-5/cmd/service/pkg/handlers/handlers.go
@@ -14,7 +14,7 @@ const (
 	noMultiplexerData      = "no multiplexer"
 )
 
-func PrefixDecorator(ctx context.Context, input chan string, output chan string) error {
+func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
 	defer close(output)
 
 	for {
@@ -39,6 +39,40 @@ func PrefixDecorator(ctx context.Context, input chan string, output chan string)
 			case <-ctx.Done():
 				return nil
 			case output <- line:
+			}
+		}
+	}
+}
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	defer func() {
+		for _, outputChannel := range outputs {
+			close(outputChannel)
+		}
+	}()
+
+	outputCount := len(outputs)
+	currentIndex := 0
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+
+		case line, okay := <-input:
+			if !okay {
+				return nil
+			}
+
+			if outputCount == 0 {
+				continue
+			}
+
+			select {
+			case <-ctx.Done():
+				return nil
+			case outputs[currentIndex%outputCount] <- line:
+				currentIndex++
 			}
 		}
 	}
