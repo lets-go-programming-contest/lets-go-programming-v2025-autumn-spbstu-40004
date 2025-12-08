8dde236a89a69bda1661093b694bd48bead505b8
15446-rus75 <vladabramov06@yandex.ru>
2025-12-08 19:16:50 +0300

    [TASK-6] global fixes

--- vladislav.abramov/task-6/internal/wifi/wifi.go
+++ vladislav.abramov/task-6/internal/wifi/wifi.go
@@ -1,31 +1,50 @@
 package wifi
 
 import (
-	"github.com/mdlayher/wifi"
-	"net"
+    "fmt"
+    "net"
+
+    "github.com/mdlayher/wifi"
 )
 
-type WiFi interface {
-	Interfaces() ([]*wifi.Interface, error)
+type WiFiHandle interface {
+    Interfaces() ([]*wifi.Interface, error)
 }
 
 type WiFiService struct {
-	WiFi WiFi
+    WiFi WiFiHandle
 }
 
-func New(wifi WiFi) WiFiService {
-	return WiFiService{WiFi: wifi}
+func New(wifi WiFiHandle) WiFiService {
+    return WiFiService{WiFi: wifi}
 }
 
 func (service WiFiService) GetAddresses() ([]net.HardwareAddr, error) {
-	interfaces, err := service.WiFi.Interfaces()
-	if err != nil {
-		return nil, err
-	}
+    interfaces, err := service.WiFi.Interfaces()
+    if err != nil {
+        return nil, fmt.Errorf("getting interfaces: %w", err)
+    }
 
-	var addrs []net.HardwareAddr
-	for _, iface := range interfaces {
-		addrs = append(addrs, iface.HardwareAddr)
-	}
-	return addrs, nil
+    addrs := make([]net.HardwareAddr, 0, len(interfaces))
+
+    for _, iface := range interfaces {
+        addrs = append(addrs, iface.HardwareAddr)
+    }
+
+    return addrs, nil
+}
+
+func (service WiFiService) GetNames() ([]string, error) {
+    interfaces, err := service.WiFi.Interfaces()
+    if err != nil {
+        return nil, fmt.Errorf("getting interfaces: %w", err)
+    }
+
+    names := make([]string, 0, len(interfaces))
+
+    for _, iface := range interfaces {
+        names = append(names, iface.Name)
+    }
+
+    return names, nil
 }
