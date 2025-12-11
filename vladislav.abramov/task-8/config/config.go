0b92c3bcb2f4fc8e4650f5f10a563301deb70a2d
15446-rus75 <vladabramov06@yandex.ru>
2025-12-11 19:05:47 +0300

    [TASK-8] ya fixes

--- vladislav.abramov/task-8/config/config.go
+++ vladislav.abramov/task-8/config/config.go
@@ -1,5 +1,9 @@
 package config
 
+import "errors"
+
+var ErrUnmarshal = errors.New("can't unmarshal yaml")
+
 type Config struct {
 	Environment string `yaml:"environment"`
 	LogLevel    string `yaml:"log_level"`
--- vladislav.abramov/task-8/config/dev.go
+++ vladislav.abramov/task-8/config/dev.go
@@ -2,13 +2,10 @@ package config
 
 import (
 	_ "embed"
-	"errors"
 	"gopkg.in/yaml.v3"
 )
 
 var devConfig []byte
-
-var ErrUnmarshal = errors.New("can't unmarshal yaml")
 
 func Load() (*Config, error) {
 	var cfg Config
