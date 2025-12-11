55430442548bb553e3d2aca5afffa7d1e2f75148
15446-rus75 <vladabramov06@yandex.ru>
2025-12-11 19:30:31 +0300

    [TASK-8] final fixes

--- vladislav.abramov/task-8/config/config.go
+++ vladislav.abramov/task-8/config/config.go
@@ -1,6 +1,34 @@
 package config
 
+import (
+	_ "embed"
+	"errors"
+	"gopkg.in/yaml.v3"
+)
+
+var ErrUnmarshal = errors.New("can't unmarshal yaml")
+
 type Config struct {
 	Environment string `yaml:"environment"`
 	LogLevel    string `yaml:"log_level"`
+}
+
+//go:embed dev.yaml
+var devConfig []byte
+
+//go:embed prod.yaml
+var prodConfig []byte
+
+func Load() (*Config, error) {
+	configData := prodConfig
+
+	return loadConfig(configData)
+}
+
+func loadConfig(data []byte) (*Config, error) {
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, ErrUnmarshal
+	}
+	return &cfg, nil
 }
