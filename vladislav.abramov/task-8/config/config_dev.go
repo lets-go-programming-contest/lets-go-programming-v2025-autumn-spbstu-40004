3abbe52a8064ea6aec001ee47bcbeb50c4be0590
15446-rus75 <vladabramov06@yandex.ru>
2025-12-11 19:30:31 +0300

    [TASK-8] final fixes

--- vladislav.abramov/task-8/config/config_dev.go
+++ vladislav.abramov/task-8/config/config_dev.go
@@ -2,23 +2,6 @@
 
 package config
 
-import (
-	"errors"
-
-	_ "embed"
-	"gopkg.in/yaml.v3"
-)
-
-//go:embed dev.yaml
-var devConfig []byte
-
-var ErrUnmarshal = errors.New("cant unmarshal yaml")
-
 func Load() (*Config, error) {
-	var cfg Config
-	if err := yaml.Unmarshal(devConfig, &cfg); err != nil {
-		return nil, ErrUnmarshal
-	}
-
-	return &cfg, nil
+	return loadConfig(devConfig)
 }
--- vladislav.abramov/task-8/config/config_prod.go
+++ vladislav.abramov/task-8/config/config_prod.go
@@ -2,23 +2,6 @@
 
 package config
 
-import (
-	_ "embed"
-	"errors"
-
-	"gopkg.in/yaml.v3"
-)
-
-//go:embed prod.yaml
-var prodConfig []byte
-
-var ErrUnmarshal = errors.New("cant unmarshal yaml")
-
 func Load() (*Config, error) {
-	var cfg Config
-	if err := yaml.Unmarshal(prodConfig, &cfg); err != nil {
-		return nil, ErrUnmarshal
-	}
-
-	return &cfg, nil
+	return loadConfig(prodConfig)
 }
