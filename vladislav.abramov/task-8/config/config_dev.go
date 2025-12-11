707787a42bc3c4245542552d6da9f11d4ee3d715
15446-rus75 <vladabramov06@yandex.ru>
2025-12-11 18:56:07 +0300

    [TASK-8] some logic fixes

--- vladislav.abramov/task-8/config/config_dev.go
+++ vladislav.abramov/task-8/config/config_dev.go
@@ -1,26 +1,5 @@
 package config
 
-import (
-	"gopkg.in/yaml.v3"
-)
-
-type Config struct {
-	Environment string `yaml:"environment"`
-	LogLevel    string `yaml:"log_level"`
-}
-
 func Load() *Config {
 	return loadConfig("dev.yaml")
-}
-
-func loadConfig(filename string) *Config {
-	data, err := devYAML.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
-	var cfg Config
-	if err := yaml.Unmarshal(data, &cfg); err != nil {
-		panic(err)
-	}
-	return &cfg
 }
--- vladislav.abramov/task-8/config/embed.go
+++
@@ -1,7 +0,0 @@
-package config
-
-import "embed"
-
-var prodYAML embed.FS
-
-var devYAML embed.FS
