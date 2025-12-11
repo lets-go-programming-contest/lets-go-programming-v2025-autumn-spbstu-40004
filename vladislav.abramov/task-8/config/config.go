905653a1060c2e5ca9dc0ed35d29317d6833d13d
15446-rus75 <vladabramov06@yandex.ru>
2025-12-11 19:10:00 +0300

    [TASK-8] yaya logic fixes

--- vladislav.abramov/task-8/config/config.go
+++ vladislav.abramov/task-8/config/config.go
@@ -1,10 +1,32 @@
 package config
 
-import "errors"
+import (
+	_ "embed"
+	"errors"
+	"gopkg.in/yaml.v3"
+)
 
 var ErrUnmarshal = errors.New("can't unmarshal yaml")
 
 type Config struct {
 	Environment string `yaml:"environment"`
 	LogLevel    string `yaml:"log_level"`
+}
+
+var devConfig []byte
+
+var prodConfig []byte
+
+func Load() (*Config, error) {
+	configData := prodConfig
+
+	return loadConfig(configData)
+}
+
+func loadConfig(data []byte) (*Config, error) {
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, ErrUnmarshal
+	}
+	return &cfg, nil
 }
