9cff97716fdae0b5f7d567505b5f610226b12187
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2025-12-12 17:25:40 +0300

    [TASK-6] %v -> %s, testifyLint

--- veniamin.belyaev/task-6/internal/wifi/wifi_test.go
+++ veniamin.belyaev/task-6/internal/wifi/wifi_test.go
@@ -28,7 +28,7 @@ func TestGetAddresses(t *testing.T) {
 
 	addresses, err := service.GetAddresses()
 
-	assert.NoErrorf(t, err, "get addresses failed: %v")
+	assert.NoErrorf(t, err, "get addresses failed: %s")
 	assert.Len(t, addresses, len(interfaces))
 	for index, elem := range interfaces {
 		assert.Equal(t, elem.HardwareAddr, addresses[index])
@@ -69,7 +69,7 @@ func TestGetNames(t *testing.T) {
 
 	names, err := service.GetNames()
 
-	assert.NoErrorf(t, err, "get addresses failed: %v")
+	assert.NoErrorf(t, err, "get addresses failed: %s")
 	assert.Len(t, names, len(interfaces))
 	for index, elem := range interfaces {
 		assert.Equal(t, elem.Name, names[index])
