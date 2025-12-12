05dfc9cfeb98bce34ee397404749e64d878c2e70
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2025-12-12 17:18:56 +0300

    [TASK-6] Added self-written tests for wifi

---
+++ veniamin.belyaev/task-6/internal/wifi/wifi_test.go
@@ -0,0 +1,88 @@
+package wifi_test
+
+import (
+	"errors"
+	"net"
+
+	wifiInternal "github.com/belyaevEDU/task-6/internal/wifi"
+	"github.com/mdlayher/wifi"
+	"github.com/stretchr/testify/assert"
+
+	"testing"
+)
+
+var errGettingInterfaces = errors.New("getting interfaces")
+
+func TestGetAddresses(t *testing.T) {
+	mockWifi := new(WiFiHandle)
+	service := wifiInternal.New(mockWifi)
+
+	interfaces := []*wifi.Interface{
+		{HardwareAddr: net.HardwareAddr{0x52, 0x69, 0x48}},
+		{HardwareAddr: net.HardwareAddr{0xAA, 0xBB, 0xCC}},
+	}
+
+	mockWifi.On("Interfaces").Return(interfaces, nil)
+
+	addresses, err := service.GetAddresses()
+
+	assert.NoErrorf(t, err, "get addresses failed: %v")
+	assert.Len(t, addresses, len(interfaces))
+	for index, elem := range interfaces {
+		assert.Equal(t, elem.HardwareAddr, addresses[index])
+	}
+
+	mockWifi.AssertExpectations(t)
+}
+
+func TestGetAddressesInterfacesError(t *testing.T) {
+	mockWifi := new(WiFiHandle)
+	service := wifiInternal.New(mockWifi)
+
+	mockWifi.On("Interfaces").Return(nil, errGettingInterfaces)
+
+	addresses, err := service.GetAddresses()
+
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), errGettingInterfaces.Error())
+	assert.Nil(t, addresses)
+
+	mockWifi.AssertExpectations(t)
+}
+
+func TestGetNames(t *testing.T) {
+	mockWifi := new(WiFiHandle)
+	service := wifiInternal.New(mockWifi)
+
+	interfaces := []*wifi.Interface{
+		{Name: "en0"},
+		{Name: "bridge0"},
+	}
+
+	mockWifi.On("Interfaces").Return(interfaces, nil)
+
+	names, err := service.GetNames()
+
+	assert.NoErrorf(t, err, "get addresses failed: %v")
+	assert.Len(t, names, len(interfaces))
+	for index, elem := range interfaces {
+		assert.Equal(t, elem.Name, names[index])
+	}
+
+	mockWifi.AssertExpectations(t)
+}
+
+func TestGetNamesInterfacesError(t *testing.T) {
+	mockWifi := new(WiFiHandle)
+	service := wifiInternal.New(mockWifi)
+
+	mockWifi.On("Interfaces").Return(nil, errGettingInterfaces)
+
+	names, err := service.GetNames()
+
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), errGettingInterfaces.Error())
+	assert.Nil(t, names)
+
+	mockWifi.AssertExpectations(t)
+}
