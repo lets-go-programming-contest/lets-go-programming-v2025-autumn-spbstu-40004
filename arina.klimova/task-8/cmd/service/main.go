954e1a0c0d4c1a1d66a79558fc0499243d56299a
arinaklimova <arina.klim.06@mail.ru>
2025-12-20 14:20:38 +0300

    [TASK-8] add main.go

--- arina.klimova/task-8/cmd/service/main.go
+++ arina.klimova/task-8/cmd/service/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/arinaklimova/task-8/config"
 )
 
 func main() {
-	fmt.Println("task-8")
+	config, err := config.Load()
+	if err != nil {
+		fmt.Println("Error:", err)
+
+		return
+	}
+
+	fmt.Print(config.Environment, " ", config.LogLevel)
 }
