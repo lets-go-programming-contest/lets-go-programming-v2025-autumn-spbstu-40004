a6650f5d62efc2558f908793a673fabd2e33d9a6
belyaevEDU <belyaev2.vv@edu.spbstu.ru>
2025-12-22 18:30:28 +0300

    [TASK-8] Added implementation for main()

--- veniamin.belyaev/task-8/cmd/service/main.go
+++ veniamin.belyaev/task-8/cmd/service/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	Config "github.com/belyaevEDU/task-8/internal/config"
+)
+
+func main() {
+	config, err := Config.Parse()
+
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	fmt.Println(config.Environment + " " + config.LogLevel)
 }
