03ab27dc4481d3c4d64aa0df35a40cc95c5234e3
ZakirovMS <mz.08.08.06@gmail.com>
2025-12-22 23:44:14 +0300

    [TASK-8] Main implementation

--- marat.zakirov/task-8/cmd/service/main.go
+++ marat.zakirov/task-8/cmd/service/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/ZakirovMS/task-8/config"
+)
+
+func main() {
+	cfg, err := config.Get()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s %s\n", cfg.Environment, cfg.LogLevel)
 }
