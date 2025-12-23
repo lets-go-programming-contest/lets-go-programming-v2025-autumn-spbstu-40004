f9555350825a5524b33256f80daadeb5e3ff1206
Cuathimbong <egvika20051@gmail.com>
2025-12-23 19:07:00 +0300

    [TASK-2-2] Lint fixes 6

--- egor.kazak/task-2-2/internal/heaputil/int_heap.go
+++ egor.kazak/task-2-2/internal/heaputil/int_heap.go
@@ -6,7 +6,7 @@ type IntHeap []int
 
 func (h *IntHeap) Len() int { return len(*h) }
 
-func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] } // max-heap
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
 
 func (h *IntHeap) Swap(i, j int) {
 	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
@@ -48,5 +48,6 @@ func Pop(h *IntHeap) int {
 	if num, ok := item.(int); ok {
 		return num
 	}
-	panic("heaputil: Pop returned non-int value")
+
+	panic("heaputil: Pop returned non-int value") //nolint:wsl
 }
