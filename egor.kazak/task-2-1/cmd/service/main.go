e28d3189d22c42bdc456460aa3e272e9cbc8809c
Boing-Blash <134197197+CuatHimBong@users.noreply.github.com>
2025-12-24 09:25:43 +0300

    [TASK-2-1] (#66)
    * [TASK-2-1] Finally fixed the problem
    
    * [TASK-2-1] Fixed main
    
    * [TASK-2-1] Silly error fix
    
    * [TASK-2-1] Lint fix
    
    * [TASK-2-1] Lint fix number 2
    
    * [TASK-2-1] Lint fix number 3
    
    * [TASK-2-1] Lint fix number 4
    
    * [TASK-2-1] Lint fix number 5
    
    * [TASK-2-1] Lint fix number 6...
    
    * [TASK-2-1] Lint fix number 7...
    
    * [TASK-2-1] Lint fix number 7...
    
    * [TASK-2-1] Lint fix number 9...
    
    * [TASK-2-1] Lint fix number 10...
    
    * [TASK-2-1] Lint fix number 11 ahh...
    
    * [TASK-2-1] Lint fix and trying to adapt tests
    
    * [TASK-2-1] Understood the task and remade the code. Preparing for lint fixes
    
    * [TASK-2-1] Hope for a final fix
    
    * [TASK-2-1] Nah, another fix
    
    * [TASK-2-1] Another fix
    
    * [TASK-2-1] Another fix 2
    
    * [TASK-2-1] Another fix 2
    
    * [TASK-2-1] Another fix 4
    
    * [TASK-2-1] Var changes
    
    * [TASK-2-1] Var changes 2
    
    * [TASK-2-1] Fixes
    
    * [TASK-2-1] Build fix
    
    * [TASK-2-1] Fixed directories
    
    * [TASK-2-1] Lint fixes
    
    * [TASK-2-1] Lint fixes 2
    
    * [TASK-2-1] Lint fixes 3
    
    * [TASK-2-1] Lint fixes 4
    
    * [TASK-2-1] Some changes
    
    * [TASK-2-1] Some changes 2

---
+++ egor.kazak/task-2-1/cmd/service/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	temp "task-2-1/internal/temperature"
+)
+
+func main() {
+	var count int
+	if _, err := fmt.Scan(&count); err != nil {
+		log.Fatal(fmt.Errorf("failed to read count: %w", err))
+	}
+
+	for range count {
+		var constraints int
+		if _, err := fmt.Scan(&constraints); err != nil {
+			log.Fatal(fmt.Errorf("failed to read constraints count: %w", err))
+		}
+
+		tempRange := temp.NewDefaultRange()
+
+		for range constraints {
+			var (
+				operator string
+				temp     int
+			)
+
+			if _, err := fmt.Scan(&operator, &temp); err != nil {
+				log.Fatal(fmt.Errorf("failed to read constraint: %w", err))
+			}
+
+			if err := tempRange.ApplyConstraint(operator, temp); err != nil {
+				log.Fatal(fmt.Errorf("invalid constraint %q %d: %w", operator, temp, err))
+			}
+
+			if value, ok := tempRange.Current(); ok {
+				fmt.Println(value)
+			} else {
+				fmt.Println(-1)
+			}
+		}
+	}
+}
---
+++ egor.kazak/task-2-1/internal/temperature/range.go
@@ -0,0 +1,58 @@
+package temperature
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	DefaultMin = 15
+	DefaultMax = 30
+)
+
+type Range struct {
+	min int
+	max int
+}
+
+var (
+	ErrInvalidRange  = errors.New("invalid range: min > max")
+	ErrUnsupportedOp = errors.New("unsupported operator")
+)
+
+func NewRange(minVal, maxVal int) (*Range, error) {
+	if minVal > maxVal {
+		return nil, fmt.Errorf("%w: %d > %d", ErrInvalidRange, minVal, maxVal)
+	}
+
+	return &Range{min: minVal, max: maxVal}, nil
+}
+
+func NewDefaultRange() *Range {
+	return &Range{min: DefaultMin, max: DefaultMax}
+}
+
+func (r *Range) ApplyConstraint(operator string, temperature int) error {
+	switch operator {
+	case ">=":
+		if temperature > r.min {
+			r.min = temperature
+		}
+	case "<=":
+		if temperature < r.max {
+			r.max = temperature
+		}
+	default:
+		return fmt.Errorf("%w: %q", ErrUnsupportedOp, operator)
+	}
+
+	return nil
+}
+
+func (r *Range) Current() (int, bool) {
+	if r.min <= r.max {
+		return r.min, true
+	}
+
+	return 0, false
+}
