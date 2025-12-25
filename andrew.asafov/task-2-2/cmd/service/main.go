a75cae33046c8cc00be796b8382b66d09de58dd8
shycoshy <neforscist@gmail.com>
2025-12-25 17:32:23 +0300

    [TASK-2-2] (#76)
    * [TASK-2-2] initial commit
    
    * [TASK-2-2] fix no.1
    
    * [TASK-2-2] fix no.2
    
    * [TASK-2-2] fix no.3
    
    * [TASK-2-2] fix no.4
    
    * [TASK-2-2] fix no.5
    
    * [TASK-2-2] fix no.6

---
+++ andrew.asafov/task-2-2/cmd/service/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type MaxHeap []int
+
+func (h *MaxHeap) Len() int           { return len(*h) }
+func (h *MaxHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *MaxHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *MaxHeap) Push(x interface{}) {
+	value, ok := x.(int)
+	if !ok {
+		return
+	}
+
+	*h = append(*h, value)
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+
+	*h = old[0 : n-1]
+
+	return x
+}
+
+func main() {
+	var dishCount, kthPreference int
+
+	_, err := fmt.Scan(&dishCount)
+	if err != nil {
+		return
+	}
+
+	arr := make([]int, dishCount)
+	for i := range dishCount {
+		_, err = fmt.Scan(&arr[i])
+		if err != nil {
+			return
+		}
+	}
+
+	_, err = fmt.Scan(&kthPreference)
+	if err != nil {
+		return
+	}
+
+	maxHeap := &MaxHeap{}
+	heap.Init(maxHeap)
+
+	for _, value := range arr {
+		heap.Push(maxHeap, value)
+	}
+
+	var result int
+
+	for range kthPreference {
+		popped := heap.Pop(maxHeap)
+		value, ok := popped.(int)
+
+		if !ok {
+			return
+		}
+
+		result = value
+	}
+
+	fmt.Println(result)
+}
